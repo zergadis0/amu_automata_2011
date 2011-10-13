2fe6952cfb7132b604da1565690207461c3b77b5
Filip Gralinski <filipg@ceti.pl>
2011-10-13 11:39:32 +0200

    dodanie metody intersect

---
+++ daut-automata-base/src/main/java/pl/edu/amu/wmi/daut/base/EmptyTransitionLabel.java
@@ -0,0 +1,26 @@
+package pl.edu.amu.wmi.daut.base;
+
+/**
+ * Puste przejście (nie epsilon-przejście!).
+ *
+ * Używane do sygnalizowania braku przejścia.
+ */
+class EmptyTransitionLabel extends TransitionLabel {
+
+    public boolean canBeEpsilon() {
+        return false;
+    }
+
+    public boolean canAcceptCharacter(char c) {
+        return false;
+    }
+
+    public boolean isEmpty() {
+        return true;
+    }
+
+    protected TransitionLabel intersectWith(TransitionLabel label) {
+        // przecięcie pustego z czymkolwiek daje oczywiście pustą etykietę
+        return this;
+    }
+}
