ef2c999c5388450e49c2c6a1b709a752f7c79153
kacper <kacper@kacper>
2011-10-18 18:21:25 +0200

    Powrot do wersji poprzedniej bez komentarza

--- daut-automata-base/src/main/java/pl/edu/amu/wmi/daut/base/AutomatonSpecification.java
+++ daut-automata-base/src/main/java/pl/edu/amu/wmi/daut/base/AutomatonSpecification.java
@@ -11,7 +11,6 @@ import java.util.List;
  * odpowiadających na pytanie, czy automat akceptuje napis, czy nie),
  * tylko "zawartość" automatu.
  */
-//modyfikuje
 abstract class AutomatonSpecification {
 
     // metody "budujące" automat
