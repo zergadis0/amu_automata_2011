5b3dc28a8a544498c5400ccf98962bac92437708
unknown <s362625@.labs.wmi.amu.edu.pl>
2011-10-20 14:37:06 +0200

    Dodanie klasy Operations - Issue 110

---
+++ daut-automata-base/src/main/java/pl/edu/amu/wmi/daut/base/Operations.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pl.edu.amu.wmi.daut.base;
+
+public class Operations {
+    /**
+     * Opis metody.
+     * @return AutomatonSpecification costam
+     */
+    public AutomatonSpecification complementLanguageAutomaton(AutomatonSpecification visitor){
+        return visitor;
+    };
+}
