014f2dfb8e34d3e82d8ba88368be5f632d379c9c
unknown <s347815@.labs.wmi.amu.edu.pl>
2011-10-21 10:06:55 +0200

    Stub metody i klasy. Test dzialania.

---
+++ daut-automata-base/src/main/java/pl/edu/amu/wmi/daut/base/AutomataOperations.java
@@ -0,0 +1,17 @@
+package pl.edu.amu.wmi.daut.base;
+
+import java.util.List;
+
+/** Jakiśtam opis klasy
+ */
+public class AutomataOperations {
+    /*
+     * Opis programu
+     * 
+     * @return To bedzie ladnie widac!
+     */
+    public String languageSum(AutomatonSpecification automat1, AutomatonSpecification automat2) {
+        String costam = "abba";
+        return costam;
+    }
+}
