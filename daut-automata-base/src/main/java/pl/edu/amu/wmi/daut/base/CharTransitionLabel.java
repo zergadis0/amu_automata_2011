86b136baa634ff8e06f7e755059c1330875db827
Mateusz <biksu@biksu-Aspire-5551.(none)>
2011-10-24 20:58:56 +0200

    Sprawdzenie checkstyle #104

--- daut-automata-base/src/main/java/pl/edu/amu/wmi/daut/base/CharTransitionLabel.java
+++ daut-automata-base/src/main/java/pl/edu/amu/wmi/daut/base/CharTransitionLabel.java
@@ -4,32 +4,32 @@ package pl.edu.amu.wmi.daut.base;
  * Przejście po podanym znaku.
  */
 class CharTransitionLabel extends TransitionLabel {
-        /**
-         * Konstruuje etykiety oznaczone znakiem 'c'.
-         */
-        public CharTransitionLabel(char c) {
-            ch_ = c;
-        }
-
-        public boolean canBeEpsilon() {
-            return false;
-        }
-
-        public boolean canAcceptCharacter(char c) {
-            return c == ch_;
-        }
-
-        public boolean isEmpty() {
-            return false;
-        }
-
-        public char getChar() {
-            return ch_;
-        }
-
-        protected TransitionLabel intersectWith(TransitionLabel label) {
-            return label.canAcceptCharacter(ch_) ? this : new EmptyTransitionLabel();
-        }
-
-        private char ch_;
+    /**
+    * Konstruuje etykiety oznaczone znakiem 'c'.
+    */
+    public CharTransitionLabel(char c) {
+        ch_ = c;
     }
+
+    public boolean canBeEpsilon() {
+        return false;
+    }
+
+    public boolean canAcceptCharacter(char c) {
+        return c == ch_;
+    }
+
+    public boolean isEmpty() {
+        return false;
+    }
+
+    public char getChar() {
+        return ch_;
+    }
+
+    protected TransitionLabel intersectWith(TransitionLabel label) {
+        return label.canAcceptCharacter(ch_) ? this : new EmptyTransitionLabel();
+    }
+
+    private char ch_;
+}
