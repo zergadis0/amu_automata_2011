98cc8cc769d8ad253fb6982d28358ed1418d12dc
unknown <Administrator@.(none)>
2011-10-26 23:53:19 +0200

    proba

--- daut-automata-base/src/main/java/pl/edu/amu/wmi/daut/base/AutomatonSpecification.java
+++ daut-automata-base/src/main/java/pl/edu/amu/wmi/daut/base/AutomatonSpecification.java
@@ -90,8 +90,9 @@ abstract class AutomatonSpecification {
         for (int i = 0; i < stany.size(); i++) {
             ilosc = i + 1;
         }
-        if(ilosc != 0)
+        if(ilosc == 0)
             return false;
+        return true;
     }
     @Override
     public String toString() {
