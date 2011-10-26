c04782577001401c3d8aa7d9a8b816246f8fbeee
plnchjnck <plnchjnck3@wp.pl>
2011-10-26 23:12:07 +0300

    Update daut-automata-base/src/main/java/pl/edu/amu/wmi/daut/base/AutomatonSpecification.java

--- daut-automata-base/src/main/java/pl/edu/amu/wmi/daut/base/AutomatonSpecification.java
+++ daut-automata-base/src/main/java/pl/edu/amu/wmi/daut/base/AutomatonSpecification.java
@@ -3,34 +3,34 @@ package pl.edu.amu.wmi.daut.base;
 import java.util.List;
 
 /**
- * Klasa abstrakcyjna reprezentująca specyfikację (opis) automatu
- * (jakie są stany, przejścia, który stan jest stanem początkowym,
- * które stany są stanami akceptującymi).
- *
- * Uwaga: klasa ta nie reprezentuje działającego automatu (nie ma tu funkcji
- * odpowiadających na pytanie, czy automat akceptuje napis, czy nie),
- * tylko "zawartość" automatu.
- */
+* Klasa abstrakcyjna reprezentująca specyfikację (opis) automatu
+* (jakie są stany, przejścia, który stan jest stanem początkowym,
+* które stany są stanami akceptującymi).
+*
+* Uwaga: klasa ta nie reprezentuje działającego automatu (nie ma tu funkcji
+* odpowiadających na pytanie, czy automat akceptuje napis, czy nie),
+* tylko "zawartość" automatu.
+*/
 abstract class AutomatonSpecification {
 
     // metody "budujące" automat
 
     /**
-     * Dodaje nowy stan do automatu.
-     *
-     * Zwraca dodany stan.
-     */
+* Dodaje nowy stan do automatu.
+*
+* Zwraca dodany stan.
+*/
     public abstract State addState();
 
     /**
-     * Dodaje przejście od stanu 'from' do stanu 'to' etykietowane etykietą transitionLabel.
-     */
+* Dodaje przejście od stanu 'from' do stanu 'to' etykietowane etykietą transitionLabel.
+*/
     public abstract void addTransition(State from, State to, TransitionLabel transitionLabel);
 
     /**
-     * Dodaje przejście od stanu 'from' do nowo utworzonego stanu 'to' etykietowane etykietą
-     * transitionLabel, a następnie zwraca utworzony stan.
-     */
+* Dodaje przejście od stanu 'from' do nowo utworzonego stanu 'to' etykietowane etykietą
+* transitionLabel, a następnie zwraca utworzony stan.
+*/
     public State addTransition(State from, TransitionLabel transitionLabel) {
 
         State to = addState();
@@ -40,48 +40,46 @@ abstract class AutomatonSpecification {
     }
 
     /**
-     * Oznacza stan jako początkowy.
-     */
+* Oznacza stan jako początkowy.
+*/
     public abstract void markAsInitial(State state);
 
     /**
-     * Oznacza stan jako końcowy (akceptujący).
-     */
+* Oznacza stan jako końcowy (akceptujący).
+*/
     public abstract void markAsFinal(State state);
 
     // metody zwracające informacje o automacie
 
     /**
-     * Zwraca listę wszystkich stanów.
-     *
-     * Stany niekoniecznie muszą być zwrócone w identycznej
-     * kolejności jak były dodane.
-     */
+* Zwraca listę wszystkich stanów.
+*
+* Stany niekoniecznie muszą być zwrócone w identycznej
+* kolejności jak były dodane.
+*/
     public abstract List<State> allStates();
 
     /**
-     * Zwraca listę wszystkich przejść wychodzących ze stanu 'from'.
-     *
-     * Przejścia niekoniecznie muszą być zwrócone w identycznej
-     * kolejności jak były dodane.
-     */
+* Zwraca listę wszystkich przejść wychodzących ze stanu 'from'.
+*
+* Przejścia niekoniecznie muszą być zwrócone w identycznej
+* kolejności jak były dodane.
+*/
     public abstract List<OutgoingTransition> allOutgoingTransitions(State from);
 
     /**
-     * Zwraca stan początkowy.
-     */
+* Zwraca stan początkowy.
+*/
     public abstract State getInitialState();
 
     /**
-     * Zwraca true wgdy stan jest stanem końcowym.
-     */
+* Zwraca true wgdy stan jest stanem końcowym.
+*/
     public abstract boolean isFinal(State state);
 
-
     /**
-     * Zwraca zawartość automatu w czytelnej dla człowieka postaci String'a.
-     */
-
+* Zwraca zawartość automatu w czytelnej dla człowieka postaci String'a.
+*/
     @Override
     public String toString() {
         StringBuffer pilgrim = new StringBuffer("Automaton:\n-States: ");
@@ -93,7 +91,7 @@ abstract class AutomatonSpecification {
         for (int i = 0; i < link.size(); i++) {
             List<OutgoingTransition> listOfTrans = allOutgoingTransitions(link.get(i));
             for (int j = 0; j < listOfTrans.size(); j++) {
-                pilgrim.append("  q" + i + " -" + listOfTrans.get(j).getTransitionLabel() + "-> q");
+                pilgrim.append(" q" + i + " -" + listOfTrans.get(j).getTransitionLabel() + "-> q");
                 State target = listOfTrans.get(j).getTargetState();
                 for (int m = 0; m < link.size(); m++) {
                     if (target == link.get(m)) {
@@ -119,12 +117,12 @@ abstract class AutomatonSpecification {
         return pilgrim.toString();
     };
    /**
-     * Sprawdza, czy automat jest deterministyczny (to znaczy, czy ma
-     * przynajmniej jeden stan, czy nie zawiera epsilon-przejść (za wyjątkiem
-     * sytuacji, gdy epsilon-przejście jest jedynym sposobem wyjścia ze stanu)
-     * oraz czy przejścia z danego stanu do innych stanów odbywają się po
-     * różnych znakach).
-     */
+* Sprawdza, czy automat jest deterministyczny (to znaczy, czy ma
+* przynajmniej jeden stan, czy nie zawiera epsilon-przejść (za wyjątkiem
+* sytuacji, gdy epsilon-przejście jest jedynym sposobem wyjścia ze stanu)
+* oraz czy przejścia z danego stanu do innych stanów odbywają się po
+* różnych znakach).
+*/
     public boolean isDeterministic() {
         List<State> states = allStates();
 
@@ -155,23 +153,23 @@ abstract class AutomatonSpecification {
     }
 
     /**
-     * Dodaje przejście od stanu state z powrotem do tego samego stanu
-     * po etykiecie transitionLabel.
-     */
+* Dodaje przejście od stanu state z powrotem do tego samego stanu
+* po etykiecie transitionLabel.
+*/
     public void addLoop(State state, TransitionLabel transitionLabel) {
 
         addTransition(state, state, transitionLabel);
     }
 
     /**
-     * Zwraca obiekt typu String, który zawiera gotowy kod w języku DOT służący do
-     * przedstawienia automatu w formie graficznej, (w ubuntu pakiet
-     * graphviz). Z konsoli wywołuje się przykładowo w następujący sposób: dot
-     * -Tpng -O plik_zkodem.dot który tworzy plik-schemat zapisany w formacie
-     * png. Więcej w: man dot.
-     * 
-     * @return Kod źródłowy schematu w języku DOT.
-     */
+* Zwraca obiekt typu String, który zawiera gotowy kod w języku DOT służący do
+* przedstawienia automatu w formie graficznej, (w ubuntu pakiet
+* graphviz). Z konsoli wywołuje się przykładowo w następujący sposób: dot
+* -Tpng -O plik_zkodem.dot który tworzy plik-schemat zapisany w formacie
+* png. Więcej w: man dot.
+*
+* @return Kod źródłowy schematu w języku DOT.
+*/
     public String getDotGraph() {
 
         class DotGraph {
@@ -186,14 +184,14 @@ abstract class AutomatonSpecification {
             private void getDotGraphIntro() {
                 dotCode.append(
                         "digraph finite_state_machine {\n"
-                         + "    rankdir=LR;\n"
-                         + "    size=\"8,5\"\n"
-                         + "    node [style=filled fillcolor=\"#00ff005f\" shape = ");
+                         + " rankdir=LR;\n"
+                         + " size=\"8,5\"\n"
+                         + " node [style=filled fillcolor=\"#00ff005f\" shape = ");
                 if (isFinal(getInitialState())) dotCode.append("double");
                 dotCode.append("circle];\n"
-                               + "    \"State #" + states.indexOf(getInitialState()) + "\";\n"
-                               + "    node [shape = doublecircle style=filled "
-                               + "fillcolor=\"#00000000\"];\n    ");
+                               + " \"State #" + states.indexOf(getInitialState()) + "\";\n"
+                               + " node [shape = doublecircle style=filled "
+                               + "fillcolor=\"#00000000\"];\n ");
             }
 
             private void getDotGraphFinalStates() {
@@ -206,7 +204,7 @@ abstract class AutomatonSpecification {
 
             private void getEdgeLabel(State state, int target, String label) {
                 if (label.length() != 0) {
-                    dotCode.append("    \"State #");
+                    dotCode.append(" \"State #");
                     dotCode.append(states.indexOf(state) + "\"");
                     dotCode.append(" -> ");
                     dotCode.append("\"State #");
@@ -243,10 +241,10 @@ abstract class AutomatonSpecification {
                 }
             }
 
-            public String  getDotGraph() {
+            public String getDotGraph() {
                 getDotGraphIntro();
                 getDotGraphFinalStates();
-                dotCode.append(";\n" + "    node [shape = circle];\n" + "");
+                dotCode.append(";\n" + " node [shape = circle];\n" + "");
                 getDotGraphEdges();
                 dotCode.append("\n}\n");
                 return dotCode.toString();
@@ -268,13 +266,4 @@ abstract class AutomatonSpecification {
         }
         return sum;
     }
-
-    /**
-     * Zwraca true, gdy automat akceptuje napis pusty.
-     */
-
-    public boolean acceptEmptyWord() {
-        return isFinal(getInitialState());
-    }
-
 };
