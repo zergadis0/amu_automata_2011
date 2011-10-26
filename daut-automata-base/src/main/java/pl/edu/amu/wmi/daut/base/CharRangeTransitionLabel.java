4cafed7066bc06a5b706b410f7488e2ffa39d786
unknown <malyglod@.(none)>
2011-10-26 18:11:15 +0200

    dodanie pliku z klasa

---
+++ daut-automata-base/src/main/java/pl/edu/amu/wmi/daut/base/CharRangeTransitionLabel.java
@@ -0,0 +1,23 @@
+package pl.edu.amu.wmi.daut.base;
+import java.util.ArrayList;
+/**
+ * Klasa reprezentująca przejście po dowolnym znaku z podanego zakresu UTF-8
+ */
+class CharRangeTransitionLabel {
+
+    private char firstChar;
+    private char secondChar;
+    private ArrayList range = new ArrayList();
+
+    public CharRangeTransitionLabel(char a, char z) {
+        firstChar = a;
+        secondChar = z;
+    }
+
+    public void makeArray() {
+        while(firstChar<secondChar) {
+            range.add(firstChar);
+            firstChar++;
+        }
+    }
+};
