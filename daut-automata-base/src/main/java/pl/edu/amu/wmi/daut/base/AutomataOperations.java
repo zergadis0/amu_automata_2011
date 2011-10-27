2875a9c26852f7a82b480907ad35661c6b6e404f
Marcin Hanc <marcin@hanc>
2011-10-27 18:37:38 +0200

    Proba ustabilizowania tego stuba

--- daut-automata-base/src/main/java/pl/edu/amu/wmi/daut/base/AutomataOperations.java
+++ daut-automata-base/src/main/java/pl/edu/amu/wmi/daut/base/AutomataOperations.java
@@ -3,12 +3,12 @@ package pl.edu.amu.wmi.daut.base;
 /**
  *
  */
-class AutomataOperations {
+public class AutomataOperations {
 
     /*
      * Metoda zwracaj¹ca Automat akceptuj¹cy jêzyk bêd¹cy dope³nieniem jêzyka akceptowanego przez Automat otrzymywany "na wejœciu".
      */
-    public static AutomatonByRecursion metodaKarola(AutomatonByRecursion pierwszy) {
+    public static AutomatonSpecification metodaKarola(AutomatonSpecification pierwszy) {
         return pierwszy;
     }
 }
