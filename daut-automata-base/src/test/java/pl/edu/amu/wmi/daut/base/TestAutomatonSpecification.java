a529506bdb51281bccae8e0e8da7956ab4f5f9f8
Chevy <chevy@chevy-Lenovo.(none)>
2011-10-27 12:41:35 +0200

    ISSUE #14 - Dodano testowa klase TestAutomationSpecification. Dodano testy do metod countTransitions i countStates. By Grzegorz Bednarski

---
+++ daut-automata-base/src/test/java/pl/edu/amu/wmi/daut/base/TestAutomatonSpecification.java
@@ -0,0 +1,60 @@
+package pl.edu.amu.wmi.daut.base;
+
+import junit.framework.TestCase;
+
+public class TestAutomatonSpecification extends TestCase {
+    
+    public final void testCountStates(){
+        NaiveAutomatonSpecification spec = new NaiveAutomatonSpecification();
+        
+        //Test 1
+        assertEquals(spec.countStates(), 0);
+
+        //Test 2
+        State q0 = spec.addState();
+        assertEquals(spec.countStates(), 1);
+
+        //Test 3
+        for(int i = 1; i <= 123456; i++)
+        {
+            State q = spec.addState();
+        }
+        assertEquals(spec.countStates(), 123456 + 1);
+        
+    }
+
+    public final void testCountTransitions(){
+        NaiveAutomatonSpecification spec = new NaiveAutomatonSpecification();
+
+        //Test 1
+        assertEquals(spec.countTransitions(), 0);
+
+        //Test 2
+        State q0 = spec.addState();
+        State q1 = spec.addState();
+        State q2 = spec.addState();
+        
+        spec.addTransition(q0, q1, new CharTransitionLabel('a'));
+        spec.addTransition(q1, q2, new CharTransitionLabel('b'));
+
+        spec.markAsInitial(q0);
+        spec.markAsFinal(q2);
+
+        assertEquals(spec.countTransitions(), 2);
+
+        //Test 3
+        spec.addTransition(q2, q0, new CharTransitionLabel('c'));
+
+        assertEquals(spec.countTransitions(), 3);
+
+        //Test 4
+        spec.addTransition(q0, q2, new CharTransitionLabel('d'));
+
+        assertEquals(spec.countTransitions(), 4);
+
+        //Test 5
+        spec.addTransition(q2, q2, new CharTransitionLabel('d'));
+
+        assertEquals(spec.countTransitions(), 5);
+    }
+}
