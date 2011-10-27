8cac4ae45a1fbbb508c1ae76bf68dd599aaee7b1
Marcin Hanc <marcin@hanc>
2011-10-27 18:42:01 +0200

    Ponadto zapamietac - nie pisac polskich znakow nawet w komentarzach.

--- daut-automata-base/src/main/java/pl/edu/amu/wmi/daut/base/AutomataOperations.java
+++ daut-automata-base/src/main/java/pl/edu/amu/wmi/daut/base/AutomataOperations.java
@@ -6,7 +6,7 @@ package pl.edu.amu.wmi.daut.base;
 public class AutomataOperations {
 
     /**
-     * Metoda zwracaj¹ca Automat akceptuj¹cy jezyk bedacy dopelnieniem jezyka akceptowanego przez Automat otrzymywany "na wejsciu".
+     * Metoda zwracajaca Automat akceptujacy jezyk bedacy dopelnieniem jezyka akceptowanego przez Automat otrzymywany "na wejsciu".
      */
     public static AutomatonSpecification metodaKarola(AutomatonSpecification pierwszy) {
         return pierwszy;
