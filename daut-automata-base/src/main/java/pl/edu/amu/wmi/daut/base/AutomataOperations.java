e7ee20610c52047ad59c70bcdc41faf4782c6ad9
Marcin Hanc <marcin@hanc>
2011-10-27 21:06:44 +0200

    Skonfigurowany na nowo NetBeans + Maven. Poprawa za dlugiej linii.

--- daut-automata-base/src/main/java/pl/edu/amu/wmi/daut/base/AutomataOperations.java
+++ daut-automata-base/src/main/java/pl/edu/amu/wmi/daut/base/AutomataOperations.java
@@ -6,7 +6,8 @@ package pl.edu.amu.wmi.daut.base;
 public class AutomataOperations {
 
     /**
-     * Metoda zwracajaca Automat akceptujacy jezyk bedacy dopelnieniem jezyka akceptowanego przez Automat otrzymywany "na wejsciu".
+     * Metoda zwracajaca Automat akceptujacy jezyk bedacy dopelnieniem jezyka
+     * akceptowanego przez Automat otrzymywany "na wejsciu".
      */
     public static AutomatonSpecification metodaKarola(AutomatonSpecification pierwszy) {
         return pierwszy;
