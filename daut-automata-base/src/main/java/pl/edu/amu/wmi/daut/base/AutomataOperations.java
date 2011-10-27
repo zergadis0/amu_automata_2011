2288df8eec40803e32abb0fdb32e0c97a3ce745c
Marcin Hanc <marcin@hanc>
2011-10-27 18:17:04 +0200

    Poprawa bledow zgloszonych przez Checkstylea

--- daut-automata-base/src/main/java/pl/edu/amu/wmi/daut/base/AutomataOperations.java
+++ daut-automata-base/src/main/java/pl/edu/amu/wmi/daut/base/AutomataOperations.java
@@ -3,8 +3,11 @@ package pl.edu.amu.wmi.daut.base;
 /**
  *
  */
-public class AutomataOperations {
-    
+public final class AutomataOperations {
+
+    /*
+     * Metoda zwracaj¹ca Automat akceptuj¹cy jêzyk bêd¹cy dope³nieniem jêzyka akceptowanego przez Automat otrzymywany "na wejœciu".
+     */
     public static AutomatonByRecursion metodaKarola(AutomatonByRecursion pierwszy) {
         return pierwszy;
     }
