e56eedca2666635708322f8e0dbd247dc82d7053
unknown <Magdalena@.(none)>
2011-10-28 11:31:18 +0200

    DodanieTestu

--- daut-automata-base/src/test/java/pl/edu/amu/wmi/daut/base/TestAutomataOperations.java
+++ daut-automata-base/src/test/java/pl/edu/amu/wmi/daut/base/TestAutomataOperations.java
@@ -44,6 +44,116 @@ public class TestAutomataOperations extends TestCase {
         private char ch_;
     }
     
+    public final void testSum() {
+        
+        /*Automat A */
+        AutomatonSpecification automatonA = new NaiveAutomatonSpecification();
+
+        {
+            State q0 = automatonA.addState();
+            State q1 = automatonA.addState();
+
+            automatonA.addTransition(q0, q1, new TestTransition('a'));
+            automatonA.addLoop(q1, new TestTransition('a'));
+            automatonA.addLoop(q1, new TestTransition('b'));
+            automatonA.markAsInitial(q0);
+            automatonA.markAsFinal(q1);
+        }
+
+        /*Automat B*/
+        AutomatonSpecification automatonB = new NaiveAutomatonSpecification();
+        {
+            State q0 = automatonB.addState();
+            State q1 = automatonB.addState();
+            State q2 = automatonB.addState();
+            automatonB.addTransition(q0, q1, new TestTransition('a'));
+            automatonB.addTransition(q0, q1, new TestTransition('b'));
+            automatonB.addTransition(q1, q2, new TestTransition('a'));
+            automatonB.addTransition(q1, q2, new TestTransition('b'));
+            automatonB.markAsInitial(q0);
+            automatonB.markAsFinal(q2);
+        }
+        /*Automat C */
+        AutomatonSpecification automatonC = new NaiveAutomatonSpecification();
+        {
+            State q0 = automatonC.addState();
+
+            automatonC.addLoop(q0, new TestTransition('a'));
+            automatonC.addLoop(q0, new TestTransition('b'));
+            automatonC.addLoop(q0, new TestTransition('c'));
+            automatonC.addLoop(q0, new TestTransition('d'));
+            automatonC.markAsInitial(q0);
+            automatonC.markAsFinal(q0);
+        }
+        /* Automat D */
+        AutomatonSpecification automatonD = new NaiveAutomatonSpecification();
+        {
+            State q0 = automatonD.addState();
+            State q1 = automatonD.addState();
+            State q2 = automatonD.addState();
+            State q3 = automatonD.addState();
+
+            automatonD.addTransition(q0, q1, new TestTransition('a'));
+            automatonD.addTransition(q0, q2, new TestTransition('b'));
+            automatonD.addTransition(q1, q3, new TestTransition('a'));
+            automatonD.addTransition(q1, q2, new TestTransition('b'));
+            automatonD.addTransition(q2, q0, new TestTransition('c'));
+            automatonD.addTransition(q2, q1, new TestTransition('b'));
+            automatonD.addTransition(q2, q3, new TestTransition('a'));
+            automatonD.addTransition(q3, q2, new TestTransition('c'));
+            automatonD.addTransition(q3, q0, new TestTransition('b'));
+
+            automatonD.markAsInitial(q0);
+            automatonD.markAsFinal(q3);
+        }
+        /* Test A z B */
+        { 
+            AutomatonSpecification result = AutomataOperations.sum(automatonA, automatonB);
+            AutomatonByRecursion automaton = new AutomatonByRecursion(result);
+
+            assertTrue(automaton.accepts("aa"));
+            assertTrue(automaton.accepts("ba"));
+            assertTrue(automaton.accepts("aaaaaaaaaaaaaaaaaaaabaaaaaaaaaaaa"));
+            assertTrue(automaton.accepts("bb"));
+            assertTrue(automaton.accepts("abbbbabbbabbb"));
+            assertFalse(automaton.accepts("bbb"));
+            assertFalse(automaton.accepts("Tegomaniezakceptowac"));
+            assertFalse(automaton.accepts("baaaaaaaaaa"));
+            assertFalse(automaton.accepts("aaaaaaaaaaaaaaaxaaaaaa"));
+            assertFalse(automaton.accepts("bab"));
+        }
+        /*Test D z B */
+        {
+            AutomatonSpecification result = AutomataOperations.sum(automatonB, automatonD);
+            AutomatonByRecursion automaton = new AutomatonByRecursion(result);
+
+            assertTrue(automaton.accepts("ab"));
+            assertTrue(automaton.accepts("abbabba"));
+            assertTrue(automaton.accepts("bbbcaacba"));
+            assertTrue(automaton.accepts("aacacaca"));
+            assertTrue(automaton.accepts("aa"));
+            assertFalse(automaton.accepts("zle"));
+            assertFalse(automaton.accepts("b"));
+            assertFalse(automaton.accepts(" "));
+            assertFalse(automaton.accepts("aac"));
+        }
+        /*Test B z C */
+        {
+            AutomatonSpecification result = AutomataOperations.sum(automatonB, automatonC);
+            AutomatonByRecursion automaton = new AutomatonByRecursion(result);
+        
+
+            assertTrue(automaton.accepts("babbaccddcaaccb"));
+            assertTrue(automaton.accepts("bbaccddbaba"));
+            assertTrue(automaton.accepts("bbbcaacba"));
+            assertTrue(automaton.accepts("aaaaaaaaaaaaaaaa"));
+            assertFalse(automaton.accepts("bbaccddxbaba"));
+            assertFalse(automaton.accepts("CzyTwojProgramMackuToZaakceptuje"));
+            assertFalse(automaton.accepts(" "));
+            assertFalse(automaton.accepts("zielonosmutnaniebieskowesolapomaranczowa"));
+        }
+    }
+    
     public final void testMetodaKarola() {
         //TEST 1 Język pusty
         {
