bdd7cfa2187ff3b1fe3b809b60e9cc346879bd9a
Marcin Hanc <marcin@hanc>
2011-10-29 14:39:09 +0200

    Proba poskromienia CheckStyle przez utworzenie protected konstruktora.

--- daut-automata-base/src/main/java/pl/edu/amu/wmi/daut/base/AutomataOperations.java
+++ daut-automata-base/src/main/java/pl/edu/amu/wmi/daut/base/AutomataOperations.java
@@ -5,6 +5,9 @@ package pl.edu.amu.wmi.daut.base;
  */
 public class AutomataOperations {
 
+    protected AutomataOperations() {
+        throw new UnsupportedOperationException(); // prevents calls from subclass
+    }
     /**
      * Metoda zwracajaca Automat akceptujacy jezyk bedacy dopelnieniem jezyka
      * akceptowanego przez Automat otrzymywany "na wejsciu".
