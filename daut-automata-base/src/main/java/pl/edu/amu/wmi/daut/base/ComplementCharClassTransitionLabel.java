642447d65797a7842b473810bd3a3c244c1099de
Maximus <Maximus@150.254.107.76>
2011-11-01 15:35:23 +0100

    test jenkinsa

--- daut-automata-base/src/main/java/pl/edu/amu/wmi/daut/base/ComplementCharClassTransitionLabel.java
+++ daut-automata-base/src/main/java/pl/edu/amu/wmi/daut/base/ComplementCharClassTransitionLabel.java
@@ -69,6 +69,6 @@ public class ComplementCharClassTransitionLabel extends TransitionLabel {
     public String getString() {
         return st_;
     }
-    private Set se_;
     private String st_;
+    private Set se_;
 }
