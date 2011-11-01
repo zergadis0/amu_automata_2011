0904974cef37d373ae241a572d3f98aa1503e0b0
unknown <Administrator@.(none)>
2011-11-01 15:50:53 +0100

    Rozwiazanie poprawione zad 135

--- daut-automata-base/src/main/java/pl/edu/amu/wmi/daut/base/AutomatonSpecification.java
+++ daut-automata-base/src/main/java/pl/edu/amu/wmi/daut/base/AutomatonSpecification.java
@@ -61,15 +61,14 @@ abstract class AutomatonSpecification {
      * Oznacza stan jako początkowy.
      */
     public abstract void markAsInitial(State state);
-    
+
     /**
      * Metoda budująca 2-stanowy automat z jednym przejściem.
      */
-    public void makeOneTransitionAutomaton(char c)
-    {
-        AutomatonSpecification spec1=new NaiveAutomatonSpecification();
-        State q0=spec1.addState();
-        State q1=spec1.addState();
+    public void makeOneTransitionAutomaton(char c) {
+        AutomatonSpecification spec1 = new NaiveAutomatonSpecification();
+        State q0 = spec1.addState();
+        State q1 = spec1.addState();
         spec1.addTransition(q0, q1, new CharTransitionLabel('c'));
         spec1.markAsInitial(q0);
         spec1.markAsFinal(q1);
