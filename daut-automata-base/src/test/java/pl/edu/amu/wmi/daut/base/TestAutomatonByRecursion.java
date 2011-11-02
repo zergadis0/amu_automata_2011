d4eb3417ec857723fc3b98d9e5f70e800d88ac77
anonim <michal@michal-HP-Pavilion-dv5-Notebook-PC.(none)>
2011-11-02 12:35:25 +0100

    Test zadania #99

--- daut-automata-base/src/test/java/pl/edu/amu/wmi/daut/base/TestAutomatonByRecursion.java
+++ daut-automata-base/src/test/java/pl/edu/amu/wmi/daut/base/TestAutomatonByRecursion.java
@@ -1,87 +1,95 @@
 package pl.edu.amu.wmi.daut.base;
 
 import junit.framework.TestCase;
-
 /**
  * Test klasy AutomatonByRecursion.
  */
 public class TestAutomatonByRecursion extends TestCase {
-
     /**
-     * Prosta etykieta przejścia dla celów testowych.
-     *
-     * FIXME Powielony kod z TestNaiveAutomatonSpecification -
-     * uporządkować po zrobieniu zadania #104.
+     * Symulacja testu.
      */
-    private static class TestTransition extends TransitionLabel {
-        /**
-         * Konstruuje etykietę oznaczoną znakiem 'c'.
-         */
-        public TestTransition(char c) {
-            ch_ = c;
-        }
-
-        public boolean canBeEpsilon() {
-            return false;
-        }
-
-        public boolean canAcceptCharacter(char c) {
-            return c == ch_;
-        }
-
-        public boolean isEmpty() {
-            return false;
-        }
-
-        public char getChar() {
-            return ch_;
-        }
-
-        protected TransitionLabel intersectWith(TransitionLabel label) {
-            return label.canAcceptCharacter(ch_) ? this : new EmptyTransitionLabel();
-        }
-
-        private char ch_;
-    }
-
     public final void testSimpleAutomaton() {
-        AutomatonSpecification spec = new NaiveAutomatonSpecification();
+        /**
+         * 1 test
+         */
+        final AutomatonSpecification spec = new NaiveAutomatonSpecification();
 
-        State q0 = spec.addState();
-        State q1 = spec.addState();
+        State q0a = spec.addState();
+        State q1a = spec.addState();
 
-        spec.addTransition(q0, q1, new TestTransition('a'));
-        spec.addLoop(q1, new TestTransition('a'));
-        spec.addLoop(q1, new TestTransition('b'));
+        spec.addTransition(q0a, q1a, new CharTransitionLabel('a'));
+        spec.addLoop(q1a, new CharTransitionLabel('a'));
+        spec.addLoop(q1a, new CharTransitionLabel('b'));
 
-        spec.markAsInitial(q0);
-        spec.markAsFinal(q1);
+        spec.markAsInitial(q0a);
+        spec.markAsFinal(q1a);
 
-        AutomatonByRecursion automaton = new AutomatonByRecursion(spec);
+        final AutomatonByRecursion automaton = new AutomatonByRecursion(spec);
 
         assertTrue(automaton.accepts("ab"));
         assertFalse(automaton.accepts("bb"));
-    }
+        assertTrue(automaton.accepts("aa"));
+        /**
+         * 2 test
+         */
+        final AutomatonSpecification specA = new NaiveAutomatonSpecification();
 
-    public final void testStaticIsEvilUnlessYouKnowTheReasonTheyAre() {
-        AutomatonSpecification specA = new NaiveAutomatonSpecification();
         State q0A = specA.addState();
         State q1A = specA.addState();
-        specA.addTransition(q0A, q1A, new TestTransition('a'));
-        specA.markAsInitial(q0A);
-        specA.markAsFinal(q1A);
-        AutomatonByRecursion autA = new AutomatonByRecursion(specA);
+        State q2A = specA.addState();
+        State q3A = specA.addState();
 
-        AutomatonSpecification specB = new NaiveAutomatonSpecification();
+        specA.addTransition(q0A, q1A, new CharTransitionLabel('a'));
+        specA.addTransition(q1A, q2A, new CharTransitionLabel('b'));
+        specA.addTransition(q1A, q3A, new CharTransitionLabel('b'));
+        specA.markAsInitial(q0A);
+        specA.markAsFinal(q2A);
+
+        AutomatonByRecursion automatonA = new AutomatonByRecursion(specA);
+
+        assertTrue(automatonA.accepts("ab"));
+        assertFalse(automatonA.accepts("bb"));
+        assertTrue(automatonA.accepts("ab"));
+        /**
+         * 3 test
+         */
+        final AutomatonSpecification specB = new NaiveAutomatonSpecification();
+
         State q0B = specB.addState();
         State q1B = specB.addState();
-        specB.addTransition(q0B, q1B, new TestTransition('b'));
-        specB.markAsInitial(q0B);
-        specB.markAsFinal(q1B);
-        AutomatonByRecursion autB = new AutomatonByRecursion(specB);
+        State q2B = specB.addState();
+        State q3B = specB.addState();
+        State q4B = specB.addState();
+        State q5B = specB.addState();
+        State q6B = specB.addState();
+        State q7B = specB.addState();
+        State q8B = specB.addState();
+        State q9B = specB.addState();
+        State q10B = specB.addState();
+        State q11B = specB.addState();
 
-        assertTrue(autA.accepts("a")); 
-        assertTrue(autB.accepts("b")); 
+        specB.addTransition(q0B, q1B, new CharTransitionLabel('a'));
+        specB.addTransition(q1B, q2B, new CharTransitionLabel('b'));
+        specB.addTransition(q2B, q3B, new CharTransitionLabel('c'));
+        specB.addTransition(q3B, q4B, new CharTransitionLabel('d'));
+        specB.addTransition(q4B, q5B, new CharTransitionLabel('e'));
+        specB.addLoop(q5B, new CharTransitionLabel('f'));
+        specB.addTransition(q3B, q6B, new CharTransitionLabel('d'));
+        specB.addTransition(q6B, q7B, new CharTransitionLabel('e'));
+        specB.addTransition(q7B, q8B, new CharTransitionLabel('f'));
+        specB.addTransition(q7B, q10B, new CharTransitionLabel('f'));
+        specB.addLoop(q10B, new CharTransitionLabel('g'));
+        specB.addTransition(q8B, q9B, new CharTransitionLabel('g'));
+        specB.addTransition(q10B, q11B, new CharTransitionLabel('h'));
+
+        specB.markAsInitial(q0B);
+        specB.markAsFinal(q11B);
+
+        AutomatonByRecursion automatonB = new AutomatonByRecursion(specB);
+
+        assertTrue(automatonB.accepts("abcdefgh"));
+        assertTrue(automatonB.accepts("abcdefgh"));
+        assertFalse(automatonB.accepts("abcdefg"));
+        assertTrue(automatonB.accepts("abcdefh"));
     }
 }
-
