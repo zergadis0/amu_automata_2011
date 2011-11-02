3895d59202f1091113ab82ccdef91e090cdc427c
irmina90 <weronika.zietek@gmail.com>
2011-11-02 15:48:04 +0100

    proba clone poprawka 6

--- daut-automata-base/src/main/java/pl/edu/amu/wmi/daut/base/AutomatonSpecification.java
+++ daut-automata-base/src/main/java/pl/edu/amu/wmi/daut/base/AutomatonSpecification.java
@@ -393,37 +393,9 @@ abstract class AutomatonSpecification {
         return false;
     }
     @Override
-    public AutomatonSpecification clone(){
-       AutomatonSpecification automatonA = new NaiveAutomatonSpecification();
-        State q0 = automatonA.addState();
-        State q1 = automatonA.addState();
-        TransitionLabel a = new TransitionLabel() {
-
-            @Override
-            public boolean canBeEpsilon() {
-                throw new UnsupportedOperationException("Not supported yet.");
-            }
-
-            @Override
-            public boolean canAcceptCharacter(char c) {
-                throw new UnsupportedOperationException("Not supported yet.");
-            }
-
-            @Override
-            public boolean isEmpty() {
-                throw new UnsupportedOperationException("Not supported yet.");
-            }
-
-            @Override
-            protected TransitionLabel intersectWith(TransitionLabel label) {
-                throw new UnsupportedOperationException("Not supported yet.");
-            }
-        };
-        automatonA.addTransition(q0, q1, a);
-        automatonA.markAsInitial(q0);
-        automatonA.markAsFinal(q1); 
+    public AutomatonSpecification clone() {
         AutomatonSpecification mini = new NaiveAutomatonSpecification();
-        State q5 = mini.addState();
+        State q0 = mini.addState();
         mini.insert(q0, this);
         return mini;
     }
