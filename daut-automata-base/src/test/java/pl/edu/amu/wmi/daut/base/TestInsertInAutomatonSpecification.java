c622bda63ed662d6874395bd8f413947658f0465
tomasz <tomasz@tomasz-laptop.(none)>
2011-11-03 20:55:08 +0100

    add one test

--- daut-automata-base/src/test/java/pl/edu/amu/wmi/daut/base/TestInsertInAutomatonSpecification.java
+++ daut-automata-base/src/test/java/pl/edu/amu/wmi/daut/base/TestInsertInAutomatonSpecification.java
@@ -19,7 +19,7 @@ public class TestInsertInAutomatonSpecification extends TestCase {
             testState[i] = secondAutomaton.addState();
             secondAutomaton.addLoop(testState[i], new CharTransitionLabel((char)((int)('A')+i)));
         }
-        
+        secondAutomaton.markAsInitial(testState[0]);
         testState[14] = secondAutomaton.addState();
         for (int i=0; i <13; ++i) {
             secondAutomaton.addTransition(testState[i+1], testState[i], 
@@ -27,24 +27,33 @@ public class TestInsertInAutomatonSpecification extends TestCase {
         }
         
         baseAutomaton.insert(baseState, secondAutomaton);
-        System.out.print(baseAutomaton);
+        //System.out.print(baseAutomaton.getDotGraph());
         assertEquals(baseAutomaton.countStates() , secondAutomaton.countStates() );
-        assertEquals(baseAutomaton.countTransitions(), secondAutomaton.countTransitions());
+        assertEquals(baseAutomaton.countTransitions(), secondAutomaton.countTransitions() );
+        List<State> states= baseAutomaton.allStates();
+        for (int i=1;i<14;++i) {
+            List<OutgoingTransition> edges = baseAutomaton.allOutgoingTransitions(states.get(i));
+            assertEquals(edges.size(), 2);
+        }
      
     }
     
-    public final void testLabel() {
+    public final void testFinalState() {
         NaiveAutomatonSpecification first = new NaiveAutomatonSpecification();
         State f1 = first.addState();
         first.markAsInitial( f1 );
         
         NaiveAutomatonSpecification second = new NaiveAutomatonSpecification();
-        State s0 = second.addState();
         State s1 = second.addState();
-        second.addTransition(s0, s1, new CharTransitionLabel('a'));
+        State s2 = second.addState();
+        second.markAsInitial(s1);
+        second.markAsFinal(s2);
+        second.addTransition(s1, s2, new CharTransitionLabel('a'));
         first.insert(f1, second);
-        assertEquals(first.allOutgoingTransitions(f1).get(0).getTransitionLabel(), 
-                second.allOutgoingTransitions(s0).get(0).getTransitionLabel());
+        List<OutgoingTransition> edges = first.allOutgoingTransitions(f1);
+        assertEquals(edges.size(), 1);
+        assertEquals(first.isFinal(edges.get(0).getTargetState()), true);
+        
     }
     
     
