39db36ba9b813b1e32eb957cf7ddbc9c2d929e95
tomasz <tomasz@tomasz-laptop.(none)>
2011-11-03 19:43:04 +0100

    test

--- daut-automata-base/src/test/java/pl/edu/amu/wmi/daut/base/TestInsertInAutomatonSpecification.java
+++ daut-automata-base/src/test/java/pl/edu/amu/wmi/daut/base/TestInsertInAutomatonSpecification.java
@@ -1,5 +1,7 @@
 package pl.edu.amu.wmi.daut.base;
 
+import java.util.List;
+
 import junit.framework.TestCase;
 
 public class TestInsertInAutomatonSpecification extends TestCase {
@@ -17,8 +19,8 @@ public class TestInsertInAutomatonSpecification extends TestCase {
             testState[i] = secondAutomaton.addState();
             secondAutomaton.addLoop(testState[i], new CharTransitionLabel((char)((int)('A')+i)));
         }
-        testState[14] = secondAutomaton.addState();
         
+        testState[14] = secondAutomaton.addState();
         for (int i=0; i <13; ++i) {
             secondAutomaton.addTransition(testState[i+1], testState[i], 
                     new CharTransitionLabel( (char)((int)('a') + i)));
@@ -26,6 +28,24 @@ public class TestInsertInAutomatonSpecification extends TestCase {
         
         baseAutomaton.insert(baseState, secondAutomaton);
         System.out.print(baseAutomaton);
-        assertEquals(baseAutomaton.countStates() , secondAutomaton.countStates());
+        assertEquals(baseAutomaton.countStates() , secondAutomaton.countStates() );
+        assertEquals(baseAutomaton.countTransitions(), secondAutomaton.countTransitions());
+     
     }
+    
+    public final void testLabel() {
+        NaiveAutomatonSpecification first = new NaiveAutomatonSpecification();
+        State f1 = first.addState();
+        first.markAsInitial( f1 );
+        
+        NaiveAutomatonSpecification second = new NaiveAutomatonSpecification();
+        State s0 = second.addState();
+        State s1 = second.addState();
+        second.addTransition(s0, s1, new CharTransitionLabel('a'));
+        first.insert(f1, second);
+        assertEquals(first.allOutgoingTransitions(f1).get(0).getTransitionLabel(), 
+                second.allOutgoingTransitions(s0).get(0).getTransitionLabel());
+    }
+    
+    
 }
