d260a7a0fce292074084afe0d0ccc1d6f07e8a42
unknown <a@a-Komputer.(none)>
2011-11-03 12:55:12 +0100

    poprawienie checkstyle

--- daut-automata-base/src/main/java/pl/edu/amu/wmi/daut/base/EpsilonTransitionLabel.java
+++ daut-automata-base/src/main/java/pl/edu/amu/wmi/daut/base/EpsilonTransitionLabel.java
@@ -1,40 +1,27 @@
 package pl.edu.amu.wmi.daut.base;
 
-/**
- * Klasa Epsilon Przejścia
- */
 public class EpsilonTransitionLabel extends TransitionLabel {
 
-    public EpsilonTransitionLabel(){ };
     /**
-     * Zwraca true gdy przejście o danej etykiecie może nastąpić
-     * bez "zjedzenia" znaku z wejścia.
+     * Konstruktor domyslny
      */
+    public EpsilonTransitionLabel() { };
+
     @Override
     public boolean canBeEpsilon() {
         return true;
     };
 
-    /**
-     * Zwraca true gdy przejście może nastąpić po znaku 'c'.
-     */
     @Override
     public boolean canAcceptCharacter(char c) {
         return false;
     };
 
-    /**
-     * Zwraca true gdy przejście jest puste.
-     *
-     * Puste przejście ma specjalny charakter (nie jest to epsilon-przejście!),
-     * jest używane przez metody zwracające TransitionLabel do zaznaczenia, że
-     * nie udało się wygenerować/znaleźć żądanego przejścia.
-     */
     @Override
     public boolean isEmpty() {
         return false;
     };
-    
+
     @Override
     public String toString() {
         return "ε";
@@ -42,7 +29,7 @@ public class EpsilonTransitionLabel extends TransitionLabel {
 
     @Override
     protected TransitionLabel intersectWith(TransitionLabel label) {
-        if(label.canBeEpsilon()) {
+        if (label.canBeEpsilon()) {
             return this;
         } else {
             return new EmptyTransitionLabel();
