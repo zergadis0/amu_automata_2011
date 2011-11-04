960a8d6adc6b3407c13c0524372fb19c7a8c36e1
Shin <shin@Asus-Ubuntu.(none)>
2011-11-04 16:50:10 +0100

    Intersection

---
+++ daut-automata-base/src/main/java/pl/edu/amu/wmi/daut/base/AutomataOperations.java
@@ -0,0 +1,10 @@
+package pl.edu.amu.wmi.daut.base;
+
+public class AutomataOperations{
+
+    public AutomatonSpecification intersect(AutomatonSpecification automatonA, AutomatonSpecification automatonB){
+        AutomatonSpecification automatonC = new NaiveAutomatonSpecification();
+
+        return automatonC;
+    }
+}
