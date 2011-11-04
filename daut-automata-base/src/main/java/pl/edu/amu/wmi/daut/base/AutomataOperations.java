547bad5e70f47073b4778e41b4b334dbca8eb40d
Shin <shin@Asus-Ubuntu.(none)>
2011-11-04 17:18:06 +0100

    change

--- daut-automata-base/src/main/java/pl/edu/amu/wmi/daut/base/AutomataOperations.java
+++ daut-automata-base/src/main/java/pl/edu/amu/wmi/daut/base/AutomataOperations.java
@@ -1,10 +1,15 @@
 package pl.edu.amu.wmi.daut.base;
 
-public class AutomataOperations{
+public class AutomataOperations {
 
-    public AutomatonSpecification intersect(AutomatonSpecification automatonA, AutomatonSpecification automatonB){
+    /**
+     * Metoda zwracająca automat akceptujący przecięcie języków akceptowanych przez
+     * dwa podane automaty.
+     */
+    public AutomatonSpecification intersection(AutomatonSpecification automatonA,
+            AutomatonSpecification automatonB) {
         AutomatonSpecification automatonC = new NaiveAutomatonSpecification();
-
+        //...
         return automatonC;
     }
 }
