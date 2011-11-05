2d1c8d6fcd62120ee46afa9b915675768b03b8cf
Emilia C <turboelo.2.0@gmail.com>
2011-11-05 13:52:54 +0100

    usunieci metody acceptedwords

--- daut-automata-base/src/main/java/pl/edu/amu/wmi/daut/base/AutomatonByRecursion.java
+++ daut-automata-base/src/main/java/pl/edu/amu/wmi/daut/base/AutomatonByRecursion.java
@@ -1,7 +1,7 @@
 package pl.edu.amu.wmi.daut.base;
 
 import java.util.List;
-import java.util.ArrayList;
+
 /**
  * klasa która decyduje czy automat zaakceptuje dany napis.
  */
@@ -43,45 +43,4 @@ public final class AutomatonByRecursion implements Acceptor {
     private TransitionLabel currentLabel;
     private final AutomatonSpecification automaton;
     private boolean accept;
-    /**
-     * Metoda zwracająca listę wszystkich słów akceptowanych przez automat bez cykli.
-     * Pobiera wszystkie przejscia z bieżącego stanu,
-     * jeżeli takie istnieją to każdą po kolei etykietę porównuje z zadanym alfabetem
-     * Jeżeli etykieta akceptuje znak z alfabetu,  obecny stan nie jest stanem akceptującym
-     * dodaje do słowa symbol a poprzednie słowo usuwa, w przeciwnym wypadku tylko
-     * dodaje symbol. Jeżeli z danego stanu nie wychodzą żadne etykiety a stan ten nie jest
-     * akceptującu to napis jest usuwany
-     */
-    private List<String> acceptedWords = new ArrayList<String>();
-    /** {@inheritDoc}*/
-    public void acceptedWords(String alphabet, String word, State state) {
-        int i = 0;
-        StringBuffer buf = new StringBuffer(word);
-              List<OutgoingTransition> allOutTransitions;
-              allOutTransitions = automaton.allOutgoingTransitions(state);
-                  if (!allOutTransitions.isEmpty()) {
-                      for (OutgoingTransition transition : allOutTransitions) {
-                          currentLabel = transition.getTransitionLabel();
-                          for (i = 0; i < alphabet.length(); i++) {
-                            if (currentLabel.canAcceptCharacter(alphabet.charAt(i))) {
-                   if (!(automaton.isFinal(state)) && (automaton.getInitialState() != state)) {
-                                        acceptedWords.remove(word);
-                   }
-                                    buf.append(alphabet.charAt(i));
-                                    word = buf.toString();
-                                    acceptedWords.add(word);
-                                    acceptedWords(alphabet, word, transition.getTargetState());
-                            } else {
-                                if (!automaton.isFinal(state)) {
-                                    acceptedWords.remove(word);
-                                }
-                          }
-                      }
-                      }
-                  } else {
-                      if (!automaton.isFinal(state)) {
-                          acceptedWords.remove(word);
-                      }
-                      }
-    }
 }
