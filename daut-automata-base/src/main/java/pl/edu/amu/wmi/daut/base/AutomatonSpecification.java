5303bf16120cfff60045a13874c666fc80a8fdff
Elendilll <Arek@.(none)>
2011-11-05 10:00:18 +0100

    CheckStyle 2

--- daut-automata-base/src/main/java/pl/edu/amu/wmi/daut/base/AutomatonSpecification.java
+++ daut-automata-base/src/main/java/pl/edu/amu/wmi/daut/base/AutomatonSpecification.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.HashMap;
 import java.util.Iterator;
-import java.lang.StringBuilder;
 
 /**
  * Klasa abstrakcyjna reprezentująca specyfikację (opis) automatu
