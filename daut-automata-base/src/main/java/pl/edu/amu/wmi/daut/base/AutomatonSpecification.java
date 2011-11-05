f657e3249889bd48fc36f87d330f53d90a757c99
Marcin Hanc <marcin@hanc>
2011-11-05 11:07:07 +0100

    Usuniecie z branch Issue_110 rozwiazania zadania 137(nieobecnego jeszcze w glownym forku).

--- daut-automata-base/src/main/java/pl/edu/amu/wmi/daut/base/AutomatonSpecification.java
+++ daut-automata-base/src/main/java/pl/edu/amu/wmi/daut/base/AutomatonSpecification.java
@@ -337,17 +337,6 @@ abstract class AutomatonSpecification {
       }
     }
 
-    /**
-     * Funkcja zmieniająca pusty automat na automat akceptujący wyłącznie
-     * napis pusty.
-     */
-    public void makeEmptyStringAutomaton() {
-        State emptyState = this.addState();
-        this.addLoop(emptyState, new EpsilonTransitionLabel());
-        this.markAsInitial(emptyState);
-        this.markAsFinal(emptyState);
-    }
-
     public boolean isFull(String alphabet) {
         int index;
         if (allStates().isEmpty())
