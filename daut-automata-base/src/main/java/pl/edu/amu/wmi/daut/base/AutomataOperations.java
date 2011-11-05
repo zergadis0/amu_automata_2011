5fe0d19acfeed3b8b86d1e325a51292260e7b576
Shin <shin@Asus-Ubuntu.(none)>
2011-11-05 14:31:37 +0100

    Zmiana

--- daut-automata-base/src/main/java/pl/edu/amu/wmi/daut/base/AutomataOperations.java
+++ daut-automata-base/src/main/java/pl/edu/amu/wmi/daut/base/AutomataOperations.java
@@ -1,9 +1,41 @@
 package pl.edu.amu.wmi.daut.base;
 
-public class AutomataOperations{
+import java.util.List;
 
-    public AutomatonSpecification intersect(AutomatonSpecification automatonA, AutomatonSpecification automatonB){
+/**
+ * Klasa zwierająca operacje na automatach.
+ */
+public class AutomataOperations {
+
+    /**
+     * Metoda zwracająca automat akceptujący przecięcie języków akceptowanych przez
+     * dwa podane automaty.
+     */
+    public AutomatonSpecification intersection(AutomatonSpecification automatonA,
+            AutomatonSpecification automatonB) {
         AutomatonSpecification automatonC = new NaiveAutomatonSpecification();
+        
+        State qA = automatonA.getInitialState();
+        State qB = automatonB.getInitialState();
+        List<OutgoingTransition> lA = allOutgoingTransitions(qA);
+        List<OutgoingTransition> lB = allOutgoingTransitions(qB);
+        List<OutgoingTransition> lC;
+
+        for(State qAn: LA.()) {
+            for(State qBn: LB.()) {
+                if(qAn.intersect(qBn)) {
+                    State qC = qA...qB
+                    if(qAn.isFinal() && qBn.isFinal())
+                        qC.markAsFinal();
+                    lC.add(qC);
+                }
+            }
+        }
+        if (isFinal(qAn) && isFinal(qBn))
+            markAsFinal(qC);
+        if(lA.isEmpty() || lB.isEmpty())
+            ;
+        //koniec
 
         return automatonC;
     }
