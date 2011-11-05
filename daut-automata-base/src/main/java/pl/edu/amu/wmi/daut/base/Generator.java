b5c163a9964e229ba8d4ba46a737ed390d21781a
Emilia C <turboelo.2.0@gmail.com>
2011-11-05 13:57:42 +0100

    Dodana klada generator

---
+++ daut-automata-base/src/main/java/pl/edu/amu/wmi/daut/base/Generator.java
@@ -0,0 +1,48 @@
+package pl.edu.amu.wmi.daut.base;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Generator {
+ /**
+     * Metoda zwracająca listę wszystkich słów akceptowanych przez automat bez cykli.
+     * Pobiera wszystkie przejscia z bieżącego stanu,
+     * jeżeli takie istnieją to każdą po kolei etykietę porównuje z zadanym alfabetem
+     * Jeżeli etykieta akceptuje znak z alfabetu,  obecny stan nie jest stanem akceptującym
+     * dodaje do słowa symbol a poprzednie słowo usuwa, w przeciwnym wypadku tylko
+     * dodaje symbol. Jeżeli z danego stanu nie wychodzą żadne etykiety a stan ten nie jest
+     * akceptującu to napis jest usuwany
+     */
+    private List<String> acceptedWords = new ArrayList<String>();
+    private TransitionLabel currentLabel;
+    /** {@inheritDoc}*/
+    public void acceptedWords(AutomatonSpecification automaton, String alphabet, String word, State state) {
+        int i = 0;
+        StringBuffer buf = new StringBuffer(word);
+        List<OutgoingTransition> allOutTransitions;
+        allOutTransitions = automaton.allOutgoingTransitions(state);
+            if (!allOutTransitions.isEmpty()) {
+                for (OutgoingTransition transition : allOutTransitions) {
+                    currentLabel = transition.getTransitionLabel();
+                    for (i = 0; i < alphabet.length(); i++) {
+                        if (currentLabel.canAcceptCharacter(alphabet.charAt(i))) {
+                        if (!(automaton.isFinal(state)) && (automaton.getInitialState() != state)) {
+                                acceptedWords.remove(word);
+                        }
+                        buf.append(alphabet.charAt(i));
+                        word = buf.toString();
+                        acceptedWords.add(word);
+                        acceptedWords(automaton, alphabet, word, transition.getTargetState());
+                        } else {
+                                if (!automaton.isFinal(state)) {
+                                    acceptedWords.remove(word);
+                                }
+                        }
+                      }
+                    }
+        } else {
+                if (!automaton.isFinal(state)) {
+                    acceptedWords.remove(word);
+                }
+                }
+    }
+}
