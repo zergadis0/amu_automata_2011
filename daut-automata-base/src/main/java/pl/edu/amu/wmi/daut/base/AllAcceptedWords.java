39fe838027a8130c37ad356050dbb0885642f3a6
Elendilll <Arek@.(none)>
2011-11-06 01:05:03 +0100

    Klasa implementująca interfejs: java.util.Iterator

---
+++ daut-automata-base/src/main/java/pl/edu/amu/wmi/daut/base/AllAcceptedWords.java
@@ -0,0 +1,24 @@
+package pl.edu.amu.wmi.daut.base;
+
+/**
+ * Klasa implementująca interfejs: java.util.Iterator
+ */
+
+public class AllAcceptedWords implements java.util.Iterator {
+
+    @Override
+    public boolean hasNext() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public Object next() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void remove() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+}
