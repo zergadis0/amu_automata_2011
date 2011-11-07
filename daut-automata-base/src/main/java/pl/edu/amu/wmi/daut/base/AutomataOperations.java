dc29a586c5cc8957047b4be27115c5bc65d421a4
unknown <Magdalena@.(none)>
2011-11-07 16:05:36 +0100

    s

--- daut-automata-base/src/main/java/pl/edu/amu/wmi/daut/base/AutomataOperations.java
+++ daut-automata-base/src/main/java/pl/edu/amu/wmi/daut/base/AutomataOperations.java
@@ -8,9 +8,7 @@ public class AutomataOperations {
     public static AutomatonSpecification sum(AutomatonSpecification A, AutomatonSpecification B) {
         AutomatonSpecification automaton = new NaiveAutomatonSpecification(); {
             State q0 = automaton.addState();
-            State qk = automaton.addState();
             automaton.markAsInitial(q0);
-            automaton.markAsFinal(qk);
             Map<State, State> statesA = new HashMap<State, State>();
             Map<State, State> statesB = new HashMap<State, State>();
             for (State s : A.allStates()) {
@@ -33,16 +31,6 @@ public class AutomataOperations {
             }
             automaton.addTransition(q0, statesA.get(A.getInitialState()), new EpsilonTransitionLabel());
 	    automaton.addTransition(q0, statesB.get(B.getInitialState()), new EpsilonTransitionLabel());
-                for (State s : A.allStates()) {
-                    if (A.isFinal(s)) {
-                       automaton.addTransition(s , qk, new EpsilonTransitionLabel());
-                    }
-                }
-                for (State s : B.allStates()) {
-                    if (A.isFinal(s)) {
-                        automaton.addTransition(s , qk, new EpsilonTransitionLabel());
-                    }
-                }
         }
         return automaton;
     }
