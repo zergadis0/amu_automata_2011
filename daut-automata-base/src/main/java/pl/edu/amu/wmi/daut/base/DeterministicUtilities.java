059ff185fbd3be6d4a1a3ce5518d03bcb2524d76
Drzejzi <drzejzi@Drzejzi-laptop.(none)>
2011-11-09 00:06:17 +0100

    ja vs checkstyle part 1

--- daut-automata-base/src/main/java/pl/edu/amu/wmi/daut/base/DeterministicUtilities.java
+++ daut-automata-base/src/main/java/pl/edu/amu/wmi/daut/base/DeterministicUtilities.java
@@ -1,49 +1,43 @@
-
-
 package pl.edu.amu.wmi.daut.base;
-
 import java.util.Set;
-
 /**
  *
  * @author drzejzi
  */
-
-
 public class DeterministicUtilities {
      /**
-     * Tworzy automat deterministyczny, który akceptuje napisy ze zbioru `language` i nie akceptuje żadnych innych napisów.
-     * Automat powinien powstać przez rozbudowanie pustego automatu deterministycznego przekazanego jako argument `automaton`.
+     * Tworzy automat deterministyczny, który akceptuje napisy ze zbioru `language`
+     * i nie akceptuje żadnych innych napisów. Automat powinien powstać przez
+     * rozbudowanie pustego automatu deterministycznego przekazanego jako argument `automaton`.
      */
-	
-    public void createAutomatonForFiniteLanguage(DeterministicAutomatonSpecification automaton, Set<String> language) {
-    int SYMBOLS_COUNTER=0;
+    public void createAutomatonForFiniteLanguage(DeterministicAutomatonSpecification automaton,
+Set<String> language) {
+    int symbolsCounter = 0;
     for (String s : language) {
-    	SYMBOLS_COUNTER+=s.length();
+        symbolsCounter += s.length();
     }
-    State q[] = new State[SYMBOLS_COUNTER];
+    State q[] = new State[symbolsCounter];
     automaton.markAsInitial(q[0]);
-    int STATES_COUNTER = 0;
+    int statesCounter = 0;
     for (String s : language) {
-    	int ACTIVE_STATE = 0;
-    	for(; ACTIVE_STATE < s.length(); ACTIVE_STATE++) {
-    		boolean LEAVE = false;
-    		for (int SEARCH=0; SEARCH <= STATES_COUNTER; SEARCH++) {
-    			if (automaton.targetState(q[ACTIVE_STATE], s.charAt(ACTIVE_STATE)) != null) {
-    				ACTIVE_STATE = SEARCH;
-    				LEAVE = true;
-    				break;
-    			}
-    		}
-    	if (LEAVE == false) {
-    		STATES_COUNTER++;
-    		q[STATES_COUNTER] = automaton.addState();
-    		automaton.addTransition(q[ACTIVE_STATE], q[STATES_COUNTER], new CharTransitionLabel(s.charAt(ACTIVE_STATE)));
-    	}
-    	}
-    	automaton.markAsFinal(q[STATES_COUNTER]);
+        int activeState = 0;
+        for ( ; activeState < s.length(); activeState++) {
+            boolean leave = false;
+            for (int search = 0; search <= statesCounter; search++) {
+                if (automaton.targetState(q[activeState], s.charAt(activeState)) != null) {
+                    activeState = search;
+                    leave = true;
+                    break;
+                }
+            }
+            if (!leave) {
+                statesCounter++;
+                q[statesCounter] = automaton.addState();
+                automaton.addTransition(q[activeState], q[statesCounter],
+new CharTransitionLabel(s.charAt(activeState)));
+            }
+        }
+        automaton.markAsFinal(q[statesCounter]);
     }
-    
     }
-
 }
