845cdda69431d6eb4f99750ff0013b6db5b73008
Maciej Lewandowski <s362646@pc17e.labs.wmi.amu.edu.pl>
2011-11-09 10:51:13 +0100

    bledy niestabilnosci 3

--- daut-automata-base/src/main/java/pl/edu/amu/wmi/daut/base/NotNaiveAutomatonSpecification.java
+++ daut-automata-base/src/main/java/pl/edu/amu/wmi/daut/base/NotNaiveAutomatonSpecification.java
@@ -4,80 +4,80 @@ import java.util.List;
 import java.util.LinkedList;
 
 /**
-* Ukonkretnienie klasy abstrakcyjnej AutomatonSpecification.
-*/
-
+ * Ukonkretnienie klasy abstrakcyjnej AutomatonSpecification.
+ */
 class NotNaiveAutomatonSpecification extends AutomatonSpecification {
 
     static class NotNaiveState implements State {
+
         /**
-        * Konstruuje stan.
-        */
+         * Konstruuje stan.
+         */
         public NotNaiveState() {
-            OutgoingTransitions = null;   
+            OutgoingTransitions = null;
         }
         private LinkedList<OutgoingTransition> OutgoingTransitions = new LinkedList<OutgoingTransition>();
     }
-    
+
     /**
-    * Pomocnicza klasa reprezentujaca przejscie.
-    */
+     * Pomocnicza klasa reprezentujaca przejscie.
+     */
     private static class NotNaiveTransition {
+
         /**
-	* Konstruuje przejscie.
-	*/
+         * Konstruuje przejscie.
+         */
         public NotNaiveTransition(NotNaiveState aFrom, NotNaiveState aTo, TransitionLabel aTransitionLabel) {
             from = aFrom;
             to = aTo;
             transitionLabel = aTransitionLabel;
         }
-        
+
         /**
-	* Zwraca stan zrodlowy.
-	*/
+         * Zwraca stan zrodlowy.
+         */
         public NotNaiveState getSourceState() {
             return from;
         }
 
         /**
-	* Zwraca stan docelowy.
-	*/
+         * Zwraca stan docelowy.
+         */
         public NotNaiveState getTargetState() {
             return to;
         }
 
         /**
-	* Zwraca etykiete przejscia.
-	*/
+         * Zwraca etykiete przejscia.
+         */
         public TransitionLabel getTransitionLabel() {
             return transitionLabel;
         }
-
         private NotNaiveState from;
         private NotNaiveState to;
         private TransitionLabel transitionLabel;
     }
-    
+
     public NotNaiveState addState() {
         NotNaiveState newState = new NotNaiveState();
         allStates.add(newState);
         return newState;
     }
-    
+
     /**
-    * Dodaje przejscie
-    */
+     * Dodaje przejscie
+     */
     public void addTransition(State from, State to, TransitionLabel transitionLabel) {
         addToOutgoingList((NotNaiveState) from, (NotNaiveState) to, transitionLabel);
-    }   
-    
+    }
+
     /**
-    * Konwertuje State na NotNaiveState 
-    */
-    public void addToOutgoingList(NotNaiveState from, NotNaiveState to, TransitionLabel transitionLabel){
+     * Konwertuje State na NotNaiveState 
+     */
+    public void addToOutgoingList(NotNaiveState from, NotNaiveState to, TransitionLabel transitionLabel) {
         from.OutgoingTransitions.add(new OutgoingTransition(transitionLabel, (State) to));
     }
-    
+
     public void markAsInitial(State state) {
         initialState = (NotNaiveState) state;
     }
@@ -89,9 +89,9 @@ class NotNaiveAutomatonSpecification extends AutomatonSpecification {
     public List<State> allStates() {
         return allStates;
     }
-             
-    public List<OutgoingTransition> allOutgoingTransitions(State from) {        
-        return ((NotNaiveState)from).OutgoingTransitions;  
+
+    public List<OutgoingTransition> allOutgoingTransitions(State from) {
+        return ((NotNaiveState) from).OutgoingTransitions;
     }
 
     public State getInitialState() {
@@ -100,16 +100,16 @@ class NotNaiveAutomatonSpecification extends AutomatonSpecification {
 
     public boolean isFinal(State state) {
         for (NotNaiveState someState : finalStates) {
-            if (someState == state)
+            if (someState == state) {
                 return true;
+            }
         }
 
         return false;
     }
-    
     /**
-    * Deklaracja potrzebnych list.
-    */
+     * Deklaracja potrzebnych list.
+     */
     private LinkedList<State> allStates = new LinkedList<State>();
     private NotNaiveState initialState;
     private LinkedList<NotNaiveState> finalStates = new LinkedList<NotNaiveState>();
