945a1da77635edbe7bfa46e5bb4ff73fdd4f36b3
unknown <nitZ@Baza-nitZ.(none)>
2011-11-09 18:41:47 +0100

    NondeterministicAutomatonByThompsonApproach zmiana

--- daut-automata-base/src/test/java/pl/edu/amu/wmi/daut/base/TestAutomataOperations.java
+++ daut-automata-base/src/test/java/pl/edu/amu/wmi/daut/base/TestAutomataOperations.java
@@ -69,7 +69,7 @@ public class TestAutomataOperations extends TestCase {
             automatonB.markAsFinal(q2B);
         /* Test A z B ok */
             AutomatonSpecification result = AutomataOperations.sum(automatonA, automatonB);
-            AutomatonByStack automaton = new AutomatonByStack(result);
+            NondeterministicAutomatonByThompsonApproach automaton = new NondeterministicAutomatonByThompsonApproach(result);
             assertTrue(automaton.accepts("aa"));
             assertTrue(automaton.accepts("ba"));
             assertTrue(automaton.accepts("aaaaaaaaaaaaaaaaaaaabaaaaaaaaaaaa"));
