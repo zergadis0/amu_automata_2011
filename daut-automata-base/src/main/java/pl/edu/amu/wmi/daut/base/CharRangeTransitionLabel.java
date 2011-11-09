6ae8b20dbd6bc99f3bc0b1a7d99e0411acfcff7c
unknown <malyglod@.(none)>
2011-11-09 20:19:25 +0100

    aaaaaaaaaaaaa

--- daut-automata-base/src/main/java/pl/edu/amu/wmi/daut/base/CharRangeTransitionLabel.java
+++ daut-automata-base/src/main/java/pl/edu/amu/wmi/daut/base/CharRangeTransitionLabel.java
@@ -1,5 +1,8 @@
 package pl.edu.amu.wmi.daut.base;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Klasa reprezentująca przejście po dowolnym znaku z podanego zakresu UTF-8.
  */
@@ -28,9 +31,41 @@ class CharRangeTransitionLabel extends TransitionLabel {
     public boolean isEmpty() {
         return false;
     }
-
-    @Override
-    protected TransitionLabel intersectWith(TransitionLabel label) {
-        return this;
+    
+    public char getFirstChar() {
+        return firstChar;
     }
+    
+    public char getSecondChar() {
+        return secondChar;
+    }
+
+    protected TransitionLabel intersectWith(TransitionLabel label, TransitionLabel label2) {
+
+        if (label instanceof CharRangeTransitionLabel && label2 instanceof CharRangeTransitionLabel) {
+
+            List<Character> labelList = new ArrayList<Character>();
+            List<Character> label2List = new ArrayList<Character>();
+
+            for (char i = ((CharRangeTransitionLabel) label).getFirstChar(); i == ((CharRangeTransitionLabel) label).getSecondChar(); i++) {
+                labelList.add(i);
+            }
+
+            for (char i = ((CharRangeTransitionLabel) label2).getFirstChar(); i == ((CharRangeTransitionLabel) label2).getSecondChar(); i++) {
+                label2List.add(i);
+            }
+
+            labelList.retainAll(label2List);
+
+            return new CharRangeTransitionLabel(labelList.get(0), labelList.get(labelList.size()-1));
+
+        } else { throw new CannotDetermineIntersectionException(); } 
+
+    }
+
+	@Override
+	protected TransitionLabel intersectWith(TransitionLabel label) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 };
