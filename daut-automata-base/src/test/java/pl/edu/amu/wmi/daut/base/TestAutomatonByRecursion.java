c1dffe7dcd0d0b03470e53cb00fd9afe4ff14ea7
anonim <michal@michal-HP-Pavilion-dv5-Notebook-PC.(none)>
2011-11-10 15:39:44 +0100

    TestAutomatonByRecursion

--- daut-automata-base/src/test/java/pl/edu/amu/wmi/daut/base/TestAutomatonByRecursion.java
+++ daut-automata-base/src/test/java/pl/edu/amu/wmi/daut/base/TestAutomatonByRecursion.java
@@ -15,20 +15,22 @@ public class TestAutomatonByRecursion extends TestCase {
 
         State q0a = spec.addState();
         State q1a = spec.addState();
+        State q2a = spec.addState();
 
         spec.addTransition(q0a, q1a, new CharTransitionLabel('a'));
-        spec.addLoop(q1a, new CharTransitionLabel('a'));
-        spec.addLoop(q1a, new CharTransitionLabel('b'));
+        spec.addTransition(q1a, q2a, new EpsilonTransitionLabel());
+        spec.addTransition(q1a, q2a, new CharTransitionLabel('b'));
+        spec.addTransition(q0a, q2a, new CharTransitionLabel('c'));
 
         spec.markAsInitial(q0a);
-        spec.markAsFinal(q1a);
+        spec.markAsFinal(q2a);
 
         final AutomatonByRecursion automaton = new AutomatonByRecursion(spec);
 
-        assertTrue(automaton.accepts("ab"));
-        assertFalse(automaton.accepts("bb"));
-        assertFalse(automaton.accepts("a a"));
-        assertTrue(automaton.accepts("aa"));
+        assertFalse(automaton.accepts("a"));
+        assertFalse(automaton.accepts("ab"));
+        assertTrue(automaton.accepts("c"));
+        assertFalse(automaton.accepts("ab"));
     }
     /**
      * Trochę bardziej skomplikowany test.
@@ -43,17 +45,20 @@ public class TestAutomatonByRecursion extends TestCase {
         State q3A = specA.addState();
 
         specA.addTransition(q0A, q1A, new CharTransitionLabel('a'));
-        specA.addTransition(q1A, q2A, new CharTransitionLabel('b'));
         specA.addTransition(q1A, q3A, new CharTransitionLabel('b'));
+        specA.addTransition(q1A, q2A, new EpsilonTransitionLabel());
+        specA.addTransition(q2A, q3A, new EpsilonTransitionLabel());
+        specA.addTransition(q0A, q3A, new CharTransitionLabel('b'));
+
         specA.markAsInitial(q0A);
-        specA.markAsFinal(q2A);
+        specA.markAsFinal(q3A);
 
         AutomatonByRecursion automatonA = new AutomatonByRecursion(specA);
 
-        assertTrue(automatonA.accepts("ab"));
-        assertFalse(automatonA.accepts("ab "));
-        assertTrue(automatonA.accepts("ab"));
-        assertFalse(automatonA.accepts("a b"));
+        assertFalse(automatonA.accepts("ab"));
+        assertFalse(automatonA.accepts("a"));
+        assertTrue(automatonA.accepts("b"));
+        assertFalse(automatonA.accepts("a"));
     }
     /**
      * Najbardziej skomplikowany test.
@@ -74,18 +79,23 @@ public class TestAutomatonByRecursion extends TestCase {
         State q9B = specB.addState();
         State q10B = specB.addState();
         State q11B = specB.addState();
+        State q12B = specB.addState();
 
         specB.addTransition(q0B, q1B, new CharTransitionLabel('a'));
         specB.addTransition(q1B, q2B, new CharTransitionLabel('b'));
         specB.addTransition(q2B, q3B, new CharTransitionLabel('c'));
         specB.addTransition(q3B, q4B, new CharTransitionLabel('d'));
+        specB.addTransition(q3B, q11B, new CharTransitionLabel('d'));
         specB.addTransition(q4B, q5B, new CharTransitionLabel('e'));
+        specB.addTransition(q4B, q7B, new CharTransitionLabel('g'));
+        specB.addTransition(q4B, q12B, new CharTransitionLabel('z'));
         specB.addLoop(q5B, new CharTransitionLabel('f'));
         specB.addTransition(q3B, q6B, new CharTransitionLabel('d'));
-        specB.addTransition(q6B, q7B, new CharTransitionLabel('e'));
-        specB.addTransition(q7B, q8B, new CharTransitionLabel('f'));
-        specB.addTransition(q7B, q10B, new CharTransitionLabel('f'));
+        specB.addTransition(q6B, q9B, new CharTransitionLabel('e'));
+        specB.addTransition(q7B, q8B, new EpsilonTransitionLabel());
+        specB.addTransition(q9B, q10B, new CharTransitionLabel('f'));
         specB.addLoop(q10B, new CharTransitionLabel('g'));
+        specB.addTransition(q12B, q11B, new EpsilonTransitionLabel());
         specB.addTransition(q8B, q9B, new CharTransitionLabel('g'));
         specB.addTransition(q10B, q11B, new CharTransitionLabel('h'));
 
@@ -94,12 +104,15 @@ public class TestAutomatonByRecursion extends TestCase {
 
         AutomatonByRecursion automatonB = new AutomatonByRecursion(specB);
 
+        assertTrue(automatonB.accepts("abcd"));
         assertTrue(automatonB.accepts("abcdefgh"));
         assertTrue(automatonB.accepts("abcdefgggh"));
-        assertFalse(automatonB.accepts("abcdefgh "));
+        assertFalse(automatonB.accepts("abcdggfh"));
         assertTrue(automatonB.accepts("abcdefgh"));
-        assertFalse(automatonB.accepts("abcde fgh"));
-        assertTrue(automatonB.accepts("abcdefh"));
-        assertFalse(automatonB.accepts("abc defgh"));
+        assertFalse(automatonB.accepts("abcdg"));
+        assertTrue(automatonB.accepts("abcd"));
+        assertFalse(automatonB.accepts("abcdz"));
+        assertTrue(automatonB.accepts("abcd"));
+        assertFalse(automatonB.accepts("abcdg"));
     }
 }
