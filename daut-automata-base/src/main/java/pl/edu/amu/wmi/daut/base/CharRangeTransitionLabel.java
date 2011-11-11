114842daf34560d0bec8b1a455742116f7e11e80
unknown <malyglod@.(none)>
2011-11-11 11:57:55 +0100

    wersja z ifami

--- daut-automata-base/src/main/java/pl/edu/amu/wmi/daut/base/CharRangeTransitionLabel.java
+++ daut-automata-base/src/main/java/pl/edu/amu/wmi/daut/base/CharRangeTransitionLabel.java
@@ -51,22 +51,16 @@ class CharRangeTransitionLabel extends TransitionLabel {
             //3
             if (a1 < b1 && a2 < b2) {
                 return new CharRangeTransitionLabel(b1, a2);
-            }
-
-            //4
-            else if (a1 > b1 && a2 > b2) {
+            } else if (a1 > b1 && a2 > b2) {
                 return new CharRangeTransitionLabel(a1, b2);
-                }
 
-            //5
-            else if (a1 < b1 && a2 > b2) {
+            } else if (a1 < b1 && a2 > b2) {
                 return new CharRangeTransitionLabel(b1, b2);
-                }
 
-            //6
-            else if (a1 > b1 && a2 < b2) {
+            } else if (a1 > b1 && a2 < b2) {
                 return new CharRangeTransitionLabel(a1, a2);
-                } else { return null; }
+
+            } else { return null; }
 
         } else { throw new CannotDetermineIntersectionException(); }
 
