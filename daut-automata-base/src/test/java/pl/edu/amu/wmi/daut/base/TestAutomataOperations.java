63e007146ac1c24fc9e65e4dd0145f570c7149cb
unknown <Magdalena@.(none)>
2011-11-13 11:29:19 +0100

    dodanie testow

--- daut-automata-base/src/test/java/pl/edu/amu/wmi/daut/base/TestAutomataOperations.java
+++ daut-automata-base/src/test/java/pl/edu/amu/wmi/daut/base/TestAutomataOperations.java
@@ -82,5 +82,112 @@ public class TestAutomataOperations extends TestCase {
             assertFalse(automaton.accepts("aaaaaaaaaaaaaaaxaaaaaa"));
             assertFalse(automaton.accepts("bab"));
     }    
+    /** Test sprawdza metode Sum w AutomataOperations B i D */
+            public final void testSumBD() {
+                /*Automat B*/
+            AutomatonSpecification automatonB = new NaiveAutomatonSpecification();
+            State q0B = automatonB.addState();
+            State q1B = automatonB.addState();
+            State q2B = automatonB.addState();
+            automatonB.addTransition(q0B, q1B, new CharTransitionLabel('a'));
+            automatonB.addTransition(q0B, q1B, new CharTransitionLabel('b'));
+            automatonB.addTransition(q1B, q2B, new CharTransitionLabel('a'));
+            automatonB.addTransition(q1B, q2B, new CharTransitionLabel('b'));
+            automatonB.markAsInitial(q0B);
+            automatonB.markAsFinal(q2B);            
+                    /* Automat D */
+            AutomatonSpecification automatonD = new NaiveAutomatonSpecification();
+            State q0D = automatonD.addState();
+            State q1D = automatonD.addState();
+            State q2D = automatonD.addState();
+            State q3D = automatonD.addState();
+            automatonD.addTransition(q0D, q1D, new CharTransitionLabel('a'));
+            automatonD.addTransition(q0D, q2D, new CharTransitionLabel('b'));
+            automatonD.addTransition(q1D, q3D, new CharTransitionLabel('a'));
+            automatonD.addTransition(q1D, q2D, new CharTransitionLabel('b'));
+            automatonD.addTransition(q2D, q0D, new CharTransitionLabel('c'));
+            automatonD.addTransition(q2D, q1D, new CharTransitionLabel('b'));
+            automatonD.addTransition(q2D, q3D, new CharTransitionLabel('a'));
+            automatonD.addTransition(q3D, q2D, new CharTransitionLabel('c'));
+            automatonD.addTransition(q3D, q0D, new CharTransitionLabel('b'));
+            automatonD.markAsInitial(q0D);
+            automatonD.markAsFinal(q3D);            
+                    /*Test D z B ok */
+            AutomatonSpecification result = AutomataOperations.sum(automatonB, automatonD);
+            NondeterministicAutomatonByThompsonApproach automaton = new 
+            NondeterministicAutomatonByThompsonApproach(result);
+            assertTrue(automaton.accepts("ab"));
+            assertTrue(automaton.accepts("abbabba"));
+            assertTrue(automaton.accepts("bbbcaacba"));
+            assertTrue(automaton.accepts("aacacaca"));
+            assertTrue(automaton.accepts("aa"));
+            assertFalse(automaton.accepts("zle"));
+            assertFalse(automaton.accepts("b"));
+            assertFalse(automaton.accepts(""));
+            assertFalse(automaton.accepts("aac"));
+    }     
+            /** Test sprawdza metode Sum w AutomataOperations B i C. */
+            public final void testSumBC() {
+           /* Automat B*/
+            AutomatonSpecification automatonB = new NaiveAutomatonSpecification();
+            State q0B = automatonB.addState();
+            State q1B = automatonB.addState();
+            State q2B = automatonB.addState();
+            automatonB.addTransition(q0B, q1B, new CharTransitionLabel('a'));
+            automatonB.addTransition(q0B, q1B, new CharTransitionLabel('b'));
+            automatonB.addTransition(q1B, q2B, new CharTransitionLabel('a'));
+            automatonB.addTransition(q1B, q2B, new CharTransitionLabel('b'));
+            automatonB.markAsInitial(q0B);
+            automatonB.markAsFinal(q2B);
+        /*Automat C */
+            AutomatonSpecification automatonC = new NaiveAutomatonSpecification();
+            State q0C = automatonC.addState();
+            automatonC.addLoop(q0C, new CharTransitionLabel('a'));
+            automatonC.addLoop(q0C, new CharTransitionLabel('b'));
+            automatonC.addLoop(q0C, new CharTransitionLabel('c'));
+            automatonC.addLoop(q0C, new CharTransitionLabel('d'));
+            automatonC.markAsInitial(q0C);
+            automatonC.markAsFinal(q0C);            
+                    /*Test B z C ok */
+            AutomatonSpecification result = AutomataOperations.sum(automatonB, automatonC);
+            NondeterministicAutomatonByThompsonApproach automaton = new 
+            NondeterministicAutomatonByThompsonApproach(result);
+            assertTrue(automaton.accepts("babbaccddcaaccb"));
+            assertTrue(automaton.accepts("bbaccddbaba"));
+            assertTrue(automaton.accepts("bbbcaacba"));
+            assertTrue(automaton.accepts("aaaaaaaaaaaaaaaa"));
+            assertTrue(automaton.accepts(""));
+            assertFalse(automaton.accepts("bbaccddxbaba"));
+            assertFalse(automaton.accepts("CzyTwojProgramMackuToZaakceptuje"));
+            assertFalse(automaton.accepts("zielonosmutnaniebieskowesolapomaranczowa"));
+       }
+            /** Test sprawdza metode Sum w AutomataOperations B i E. */
+                public final void testSumBE() {                    
+                               /* Automat B*/
+            AutomatonSpecification automatonB = new NaiveAutomatonSpecification();
+            State q0B = automatonB.addState();
+            State q1B = automatonB.addState();
+            State q2B = automatonB.addState();
+            automatonB.addTransition(q0B, q1B, new CharTransitionLabel('a'));
+            automatonB.addTransition(q0B, q1B, new CharTransitionLabel('b'));
+            automatonB.addTransition(q1B, q2B, new CharTransitionLabel('a'));
+            automatonB.addTransition(q1B, q2B, new CharTransitionLabel('b'));
+            automatonB.markAsInitial(q0B);
+            automatonB.markAsFinal(q2B);
+            /*Automat E*/
+            AutomatonSpecification automatonE = new NaiveAutomatonSpecification();
+            State q0E = automatonE.addState();
+            automatonE.addTransition(q0E, q0E, new EpsilonTransitionLabel());
+            automatonE.markAsInitial(q0E);
+            automatonE.markAsFinal(q0E);
+            /*Test B z E */
+            AutomatonSpecification result = AutomataOperations.sum(automatonB, automatonE);
+            NondeterministicAutomatonByThompsonApproach automaton = new 
+            NondeterministicAutomatonByThompsonApproach(result);
+            assertTrue(automaton.accepts(""));
+            assertTrue(automaton.accepts("aa"));
+            assertFalse(automaton.accepts("bbaccddxbaba"));
+            assertFalse(automaton.accepts("aabbbaaaa"));
+        }
 }
 
