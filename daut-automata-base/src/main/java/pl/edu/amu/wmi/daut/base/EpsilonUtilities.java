a94b86794981d159fd83c57fd6d82eff28714b53
iwanicki <oliwer.iwanicki@gmail.com>
2011-11-15 22:40:27 +0100

    zmiany w EpsilonUtilities

--- daut-automata-base/src/main/java/pl/edu/amu/wmi/daut/base/EpsilonUtilities.java
+++ daut-automata-base/src/main/java/pl/edu/amu/wmi/daut/base/EpsilonUtilities.java
@@ -2,15 +2,17 @@ package pl.edu.amu.wmi.daut.base;
 
 import java.util.List;
 /**
- *
- * @author iwanicki
+ * Pomocnicza klasa
+ * do obslugi epsilonow.
  */
 public class EpsilonUtilities {
     /**
      * Metoda sprawdzajaca, czy dany automat
      * ma jakies epsilon-przejscie.
      */
-    public boolean isEpsilonTransition(AutomatonSpecification automat) {
+    protected EpsilonUtilities() throws UnsupportedOperationException {}
+
+    public static boolean isEpsilonTransition(AutomatonSpecification automat) {
         List<State> states = automat.allStates();
 
         if (states.isEmpty())
@@ -19,13 +21,14 @@ public class EpsilonUtilities {
         for (State state : states) {
             List<OutgoingTransition> transitions = automat.allOutgoingTransitions(state);
 
-            for (int i = 0; i < transitions.size(); ++i) {
-                TransitionLabel label = transitions.get(i).getTransitionLabel();
+            for (OutgoingTransition transition : transitions) {
+                TransitionLabel label = transition.getTransitionLabel();
 
                 if (label.canBeEpsilon())
                     return true;
             }
         }
         return false;
+
     }
 }
