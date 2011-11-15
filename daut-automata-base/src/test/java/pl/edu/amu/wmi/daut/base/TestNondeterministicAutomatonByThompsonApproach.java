2157e370a1c852d92e017476bdedf828232b39b3
Konrad Drygas <zashi@erilors.(none)>
2011-11-15 21:15:28 +0100

    poprawki checkstyle testow do #101

--- daut-automata-base/src/test/java/pl/edu/amu/wmi/daut/base/TestNondeterministicAutomatonByThompsonApproach.java
+++ daut-automata-base/src/test/java/pl/edu/amu/wmi/daut/base/TestNondeterministicAutomatonByThompsonApproach.java
@@ -62,25 +62,24 @@ public class TestNondeterministicAutomatonByThompsonApproach extends TestCase {
         State q1a = spec.addState();
         State q2a = spec.addState();
         State q3a = spec.addState();
-        
+
         spec.addTransition(q0a, q1a, new EpsilonTransitionLabel());
         spec.addTransition(q0a, q2a, new EpsilonTransitionLabel());
         spec.addTransition(q1a, q3a, new EpsilonTransitionLabel());
         spec.addTransition(q2a, q3a, new EpsilonTransitionLabel());
-        
-	spec.markAsInitial(q0a);
+
+        spec.markAsInitial(q0a);
         spec.markAsFinal(q3a);
 
         final NondeterministicAutomatonByThompsonApproach automaton = new NondeterministicAutomatonByThompsonApproach(spec);
 
-	assertTrue(automaton.accepts(""));  
-	assertFalse(automaton.accepts("uam"));  
+        assertTrue(automaton.accepts(""));
+        assertFalse(automaton.accepts("uam"));
     }
-    
-	 /**
-          * Trzeci test (pusty automat).
-          */
 
+    /**
+     * Trzeci test (pusty automat).
+     */
     public final void test3() {
         final AutomatonSpecification spec = new NaiveAutomatonSpecification();
 
@@ -89,13 +88,14 @@ public class TestNondeterministicAutomatonByThompsonApproach extends TestCase {
         assertFalse(automaton.accepts("cccccccccabbbbbbc"));
         assertFalse(automaton.accepts("aaaaaaaaaaa"));
         assertFalse(automaton.accepts("bcccccc"));
-        assertFalse(automaton.accepts("z")); 
+        assertFalse(automaton.accepts("z"));
     }
 
-	/**
-         * Czwarty test (Od stanu początkowego odchodzą 3 epsilon-przejścia do trzech różnych stanów.      		 * Dopiero od owych trzech stanów występują "normalne" przejścia - tj. po znaku).
-         */
-
+    /**
+     * Czwarty test (Od stanu początkowego odchodzą 3 epsilon-przejścia do 
+     * trzech różnych stanów. Dopiero od owych trzech stanów występują
+     * "normalne" przejścia - tj. po znaku).
+     */
     public final void test4() {
         final AutomatonSpecification spec = new NaiveAutomatonSpecification();
 
@@ -105,49 +105,49 @@ public class TestNondeterministicAutomatonByThompsonApproach extends TestCase {
         State q3a = spec.addState();
         State q4a = spec.addState();
         State q5a = spec.addState();
-	State q6a = spec.addState();
-	State q7a = spec.addState();
+        State q6a = spec.addState();
+        State q7a = spec.addState();
 
-	spec.addTransition(q0a, q1a, new EpsilonTransitionLabel());
-	spec.addTransition(q0a, q2a, new EpsilonTransitionLabel());
-	spec.addTransition(q0a, q3a, new EpsilonTransitionLabel());
-	spec.addTransition(q1a, q4a, new CharTransitionLabel('a'));
-	spec.addTransition(q1a, q5a, new CharTransitionLabel('a'));
-	spec.addTransition(q2a, q5a, new CharTransitionLabel('b'));
-	spec.addTransition(q3a, q5a, new CharTransitionLabel('c'));
-	spec.addTransition(q3a, q6a, new CharTransitionLabel('a'));
-	spec.addTransition(q3a, q6a, new CharTransitionLabel('b'));
-	spec.addTransition(q4a, q5a, new CharTransitionLabel('b'));
-	spec.addTransition(q4a, q7a, new CharTransitionLabel('b'));
-	spec.addTransition(q5a, q7a, new CharTransitionLabel('a'));
-	spec.addTransition(q5a, q6a, new CharTransitionLabel('a'));
-	spec.addTransition(q6a, q7a, new CharTransitionLabel('c'));
-	spec.addTransition(q6a, q5a, new CharTransitionLabel('b'));
+        spec.addTransition(q0a, q1a, new EpsilonTransitionLabel());
+        spec.addTransition(q0a, q2a, new EpsilonTransitionLabel());
+        spec.addTransition(q0a, q3a, new EpsilonTransitionLabel());
+        spec.addTransition(q1a, q4a, new CharTransitionLabel('a'));
+        spec.addTransition(q1a, q5a, new CharTransitionLabel('a'));
+        spec.addTransition(q2a, q5a, new CharTransitionLabel('b'));
+        spec.addTransition(q3a, q5a, new CharTransitionLabel('c'));
+        spec.addTransition(q3a, q6a, new CharTransitionLabel('a'));
+        spec.addTransition(q3a, q6a, new CharTransitionLabel('b'));
+        spec.addTransition(q4a, q5a, new CharTransitionLabel('b'));
+        spec.addTransition(q4a, q7a, new CharTransitionLabel('b'));
+        spec.addTransition(q5a, q7a, new CharTransitionLabel('a'));
+        spec.addTransition(q5a, q6a, new CharTransitionLabel('a'));
+        spec.addTransition(q6a, q7a, new CharTransitionLabel('c'));
+        spec.addTransition(q6a, q5a, new CharTransitionLabel('b'));
         spec.addLoop(q3a, new CharTransitionLabel('c'));
-	spec.addLoop(q6a, new CharTransitionLabel('c'));
-	spec.addLoop(q7a, new CharTransitionLabel('a'));
-	spec.addLoop(q7a, new CharTransitionLabel('b'));
-	spec.addLoop(q7a, new CharTransitionLabel('c'));
+        spec.addLoop(q6a, new CharTransitionLabel('c'));
+        spec.addLoop(q7a, new CharTransitionLabel('a'));
+        spec.addLoop(q7a, new CharTransitionLabel('b'));
+        spec.addLoop(q7a, new CharTransitionLabel('c'));
 
-	spec.markAsInitial(q0a);
+        spec.markAsInitial(q0a);
         spec.markAsFinal(q7a);
 
-	final NondeterministicAutomatonByThompsonApproach automaton = new NondeterministicAutomatonByThompsonApproach(spec);
+        final NondeterministicAutomatonByThompsonApproach automaton = new NondeterministicAutomatonByThompsonApproach(spec);
 
-	assertTrue(automaton.accepts("abc"));
-	assertTrue(automaton.accepts("ac"));
-	assertTrue(automaton.accepts("baaaaaaa"));
-	assertTrue(automaton.accepts("ccccccca"));
+        assertTrue(automaton.accepts("abc"));
+        assertTrue(automaton.accepts("ac"));
+        assertTrue(automaton.accepts("baaaaaaa"));
+        assertTrue(automaton.accepts("ccccccca"));
         assertFalse(automaton.accepts("ccc"));
-	assertFalse(automaton.accepts(""));
-	assertFalse(automaton.accepts("uam"));
+        assertFalse(automaton.accepts(""));
+        assertFalse(automaton.accepts("uam"));
     }
 
-	/**
-         * Piąty test (Do stanu początkowego prowadzą tylko epsilon przejścia. Występują pętle epsilon).      		 * 
-         */
-	
-	public final void test5() {
+    /**
+     * Piąty test (Do stanu końcowego prowadzą tylko epsilon przejścia. 
+     * Występują pętle epsilon).      		 * 
+     */
+    public final void test5() {
         final AutomatonSpecification spec = new NaiveAutomatonSpecification();
 
         State q0a = spec.addState();
@@ -155,30 +155,28 @@ public class TestNondeterministicAutomatonByThompsonApproach extends TestCase {
         State q2a = spec.addState();
         State q3a = spec.addState();
         State q4a = spec.addState();
-        
 
-	spec.addTransition(q1a, q4a, new EpsilonTransitionLabel());
-	spec.addTransition(q2a, q4a, new EpsilonTransitionLabel());
-	spec.addTransition(q3a, q4a, new EpsilonTransitionLabel());
-	spec.addTransition(q0a, q1a, new CharTransitionLabel('a'));
-	spec.addTransition(q0a, q2a, new CharTransitionLabel('b'));
-	spec.addTransition(q0a, q3a, new CharTransitionLabel('b'));
-	spec.addTransition(q1a, q2a, new CharTransitionLabel('a'));
-	spec.addTransition(q2a, q0a, new CharTransitionLabel('b'));
-	spec.addTransition(q3a, q2a, new CharTransitionLabel('c'));	
+        spec.addTransition(q1a, q4a, new EpsilonTransitionLabel());
+        spec.addTransition(q2a, q4a, new EpsilonTransitionLabel());
+        spec.addTransition(q3a, q4a, new EpsilonTransitionLabel());
+        spec.addTransition(q0a, q1a, new CharTransitionLabel('a'));
+        spec.addTransition(q0a, q2a, new CharTransitionLabel('b'));
+        spec.addTransition(q0a, q3a, new CharTransitionLabel('b'));
+        spec.addTransition(q1a, q2a, new CharTransitionLabel('a'));
+        spec.addTransition(q2a, q0a, new CharTransitionLabel('b'));
+        spec.addTransition(q3a, q2a, new CharTransitionLabel('c'));
         spec.addLoop(q1a, new CharTransitionLabel('c'));
-	spec.addLoop(q3a, new CharTransitionLabel('a'));
-	spec.addLoop(q2a, new CharTransitionLabel('b'));
-	spec.addLoop(q1a, new EpsilonTransitionLabel());
-	spec.addLoop(q3a, new EpsilonTransitionLabel());
+        spec.addLoop(q3a, new CharTransitionLabel('a'));
+        spec.addLoop(q2a, new CharTransitionLabel('b'));
+        spec.addLoop(q1a, new EpsilonTransitionLabel());
+        spec.addLoop(q3a, new EpsilonTransitionLabel());
 
-	spec.markAsInitial(q0a);
+        spec.markAsInitial(q0a);
         spec.markAsFinal(q4a);
 
-	final NondeterministicAutomatonByThompsonApproach automaton = new NondeterministicAutomatonByThompsonApproach(spec);
+        final NondeterministicAutomatonByThompsonApproach automaton = new NondeterministicAutomatonByThompsonApproach(spec);
 
-	assertTrue(automaton.accepts("b"));
-	assertFalse(automaton.accepts("uam"));
+        assertTrue(automaton.accepts("b"));
+        assertFalse(automaton.accepts("uam"));
     }
-
 }
