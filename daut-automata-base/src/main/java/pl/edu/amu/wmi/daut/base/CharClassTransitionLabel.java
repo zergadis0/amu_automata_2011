83dc1f4d2ae1d319467d691b41c344e2b7b34169
kacper <kacper@kacper>
2011-11-16 21:50:33 +0100

    Stworzylem klase

---
+++ daut-automata-base/src/main/java/pl/edu/amu/wmi/daut/base/CharClassTransitionLabel.java
@@ -0,0 +1,89 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pl.edu.amu.wmi.daut.base;
+
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+/**
+ *
+ * @author kacper
+ */
+public class CharClassTransitionLabel extends TransitionLabel {
+    
+    private SortedSet<Character> ssc = new TreeSet<Character>();
+    private String charClass;
+    
+    public CharClassTransitionLabel(String s) {
+        charClass = new String(s);
+        for(int i = 0; i < s.length(); ++i) {
+            if(s.charAt(i) == '-') { 
+                addChar(s.charAt(i-1), s.charAt(i+1));
+                i++;
+            }
+            else if((i+1)<s.length() && s.charAt(i+1) == '-' ) {
+                addChar(s.charAt(i), s.charAt(i+2));
+                i+=2;
+            }
+            else addChar(s.charAt(i));
+        }
+    }
+    
+    private void addChar(Character c1, Character c2) {
+        for(Character d = c1 ; d <= c2 ; ++d) ssc.add(d);
+    }
+    public void addChar(Character c) {
+        ssc.add(c);
+    }
+
+    @Override
+    public boolean canBeEpsilon() {
+        return false;
+    }
+
+    @Override
+    public boolean canAcceptCharacter(char c) {
+        return ssc.contains( (Character)c );
+        
+    }
+
+    @Override
+    public boolean isEmpty() {
+        if(charClass.isEmpty()) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    
+    @Override
+    public String toString() {
+        return "["+charClass+"]";
+    }
+
+    /**
+     * 
+     * @param label
+     * @return
+     */
+    @Override
+    protected TransitionLabel intersectWith(TransitionLabel label) {
+        SortedSet<Character> tmp = new TreeSet<Character>();
+        StringBuilder sb = new StringBuilder();
+        for(Character c : ssc) {
+            if(label.canAcceptCharacter(c)) {
+                tmp.add(c);
+            }
+        }
+        for(Character c : tmp) {
+            sb.append(c);
+        }
+        return new CharClassTransitionLabel(sb.toString());
+        
+    }
+    
+    
+}
