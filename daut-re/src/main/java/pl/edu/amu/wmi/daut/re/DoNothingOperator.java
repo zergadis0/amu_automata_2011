ca5e384ace078b30ad72133ec5b70a12ce398979
Drzejzi <drzejzi@Drzejzi-laptop.(none)>
2011-11-23 18:37:57 +0100

    rozw DoNothingOperator

---
+++ daut-re/src/main/java/pl/edu/amu/wmi/daut/re/DoNothingOperator.java
@@ -0,0 +1,20 @@
+package pl.edu.amu.wmi.daut.re;
+
+import pl.edu.amu.wmi.daut.base.AutomatonSpecification;
+
+/**
+ * Klasa dziedzicząca po UnaryRegexpOperator,
+ * która nic nie robi.
+ */
+public class DoNothingOperator extends UnaryRegexpOperator {
+
+    /**
+     * Nadpisana metoda, która była abstrakcyjną
+     * w UnaryRegexpOperator.
+     */
+    public AutomatonSpecification createAutomatonFromOneAutomaton(
+    AutomatonSpecification subautomaton) {
+        return null;
+    }
+
+};
