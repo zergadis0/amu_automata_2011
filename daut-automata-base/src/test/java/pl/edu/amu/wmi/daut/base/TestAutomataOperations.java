8c6842d61688cfb7604f5651338697252dfefe60
Administrator <ania@linuxmint.linuxmint-domain>
2011-11-23 11:22:16 +0100

    usuniety jeden test

--- daut-automata-base/src/test/java/pl/edu/amu/wmi/daut/base/TestAutomataOperations.java
+++ daut-automata-base/src/test/java/pl/edu/amu/wmi/daut/base/TestAutomataOperations.java
@@ -48,41 +48,6 @@ public class TestAutomataOperations extends TestCase {
     }
 
     /**
-    *drugi test.
-    */
-
-    public final void testIntersection2() {
-
-        AutomatonSpecification automatonA = new NaiveAutomatonSpecification();
-
-        State q0 = automatonA.addState();
-        State q1 = automatonA.addState();
-        automatonA.addTransition(q0, q1, new CharTransitionLabel('a'));
-        automatonA.addTransition(q0, q1, new CharTransitionLabel('b'));
-        automatonA.markAsInitial(q0);
-        automatonA.markAsFinal(q1);
-
-        AutomatonSpecification automatonB = new NaiveAutomatonSpecification();
-
-        State q10 = automatonB.addState();
-        State q11 = automatonB.addState();
-        automatonB.addTransition(q10, q11, new CharTransitionLabel('a'));
-        automatonB.addTransition(q10, q11, new CharTransitionLabel('b'));
-        automatonB.addLoop(q11, new CharTransitionLabel('b'));
-        automatonB.markAsInitial(q10);
-        automatonB.markAsFinal(q11);
-
-
-        AutomatonSpecification result = AutomataOperations.intersection(automatonA, automatonB);
-        AutomatonByRecursion automaton = new AutomatonByRecursion(result);
-
-        assertTrue(automaton.accepts("a"));
-        assertFalse(automaton.accepts(""));
-        assertFalse(automaton.accepts("baba"));
-        assertFalse(automaton.accepts("dziwne dlugie slowo"));
-    }
-
-    /**
      * Test metody intersection z AutomataOperations na automacie A z soba samym.
      */
     public final void testIntersectionWithTheSameAutomaton() {
