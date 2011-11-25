34b15ffd9ba7ed65f0ae32b959b4b0f494facec9
Drzejzi <drzejzi@gmail.com>
2011-11-25 20:50:53 +0100

    testowanie

--- daut-automata-base/src/test/java/pl/edu/amu/wmi/daut/base/TestDeterministicUtilities.java
+++ daut-automata-base/src/test/java/pl/edu/amu/wmi/daut/base/TestDeterministicUtilities.java
@@ -56,7 +56,7 @@ assertTrue(compare(language2, accepted2));
         language3.add("kok");
         language3.add("kat");
        // language3.add("");
-        //language3.add("ko");
+        language3.add("ko");
        // language3.add("k");
         //language3.add("tak");
        // language3.add("ak");
