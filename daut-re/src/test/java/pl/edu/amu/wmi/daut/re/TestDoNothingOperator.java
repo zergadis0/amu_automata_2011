07b4cd899dd75cf4c0f6ad4eb2c1bc60900b8cf0
Drzejzi <drzejzi@Drzejzi-laptop.(none)>
2011-11-28 21:01:51 +0100

    DoNothingOperator rozw + test

---
+++ daut-re/src/test/java/pl/edu/amu/wmi/daut/re/TestDoNothingOperator.java
@@ -0,0 +1,36 @@
+package pl.edu.amu.wmi.daut.re;
+
+import pl.edu.amu.wmi.daut.base.AutomatonSpecification;
+import pl.edu.amu.wmi.daut.base.NaiveAutomatonSpecification;
+
+import junit.framework.TestCase;
+
+
+/**
+ * Testy klasy DoNothingOperator.
+ */
+public class TestDoNothingOperator extends TestCase {
+
+    /**
+     * Test konstruktora DoNothingOperator.
+     */
+    public TestDoNothingOperator() {
+
+        DoNothingOperator operator = new DoNothingOperator();
+        assertNotNull(operator);
+    }
+
+    /**
+     * Test metody createAutomatonFromOneAutomaton.
+     */
+    public final void testCreateAutomatonFromOneAutomaton() {
+
+        DoNothingOperator operator = new DoNothingOperator();
+        AutomatonSpecification subautomaton = new NaiveAutomatonSpecification();
+
+        AutomatonSpecification automaton =
+                operator.createAutomatonFromOneAutomaton(subautomaton);
+    
+        assertEquals(automaton, subautomaton);
+    }
+}
