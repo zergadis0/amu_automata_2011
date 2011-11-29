cc8709187a6e823d829a706389e4bc54e07837de
Adam Beczynski <adam@beczynski>
2011-11-29 21:32:17 +0100

    SingleCharacterOperator - Relase Candidate ;)

--- daut-re/src/main/java/pl/edu/amu/wmi/daut/re/SingleCharacterOperator.java
+++ daut-re/src/main/java/pl/edu/amu/wmi/daut/re/SingleCharacterOperator.java
@@ -1,47 +1,53 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package pl.edu.amu.wmi.daut.re;
 
-import java.util.Set;
-import java.util.HashSet;
 import pl.edu.amu.wmi.daut.base.AutomatonSpecification;
-import pl.edu.amu.wmi.daut.base.DeterministicUtilities;
+import pl.edu.amu.wmi.daut.base.NaiveAutomatonSpecification;
 
 /**
  *
  * @author Adam
  */
 public class SingleCharacterOperator extends NullaryRegexpOperator {
-    
+
     char character;
-    
+
     /**
-     * Generuje automat składający się tylko z jednego znaku.
+     * Konstruktor jaki jest każdy widzi. Ten akurat pobiera chara :)
      */
     public SingleCharacterOperator(char a) {
         character = a;
     }
 
+    /**
+     * Generuje automat składający się tylko z jednego znaku.
+     */
     @Override
     public AutomatonSpecification createFixedAutomaton() {
-        AutomatonSpecification automaton = new DeterministicAutomatonSpecification();//error
-        
+
+
+
+        AutomatonSpecification automaton = new NaiveAutomatonSpecification();
+
+        automaton.makeOneTransitionAutomaton(character);
+
+
+        //generowanie automatu metodą z deterministic utilities dla jednoznakowego stringa.
+        /*
         Set<String> singleCharLanguage = new HashSet<String>();
         singleCharLanguage.add(Character.toString(character));
-        
+
         DeterministicUtilities utility = new DeterministicUtilities();
-        utility.createAutomatonForFiniteLanguage(automaton, singleCharLanguage);//error
+        utility.createAutomatonForFiniteLanguage(automaton, singleCharLanguage);//error*/
+
+        //Wersja powielająca kod z "makeOneTransitionAutomaton napisana na poczatku
         /*State initial = automaton.addState();
         State characterState = automaton.addState();
         automaton.markAsInitial(initial);
         automaton.markAsFinal(characterState);
         TransitionLabel characterTransition = new CharTransitionLabel(a);
         automaton.addTransition(initial, characterState, characterTransition);*/
-        
-        return automaton;
-                
+
+        return automaton;           
     }
-    
 }
