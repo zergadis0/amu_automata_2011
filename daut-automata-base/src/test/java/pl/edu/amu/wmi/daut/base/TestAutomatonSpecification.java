7e63eee5a6fc96b66970095fdae92fb1b7cf06fa
Paweł Cińcio <pawel@cincio>
2011-11-30 00:36:25 +0100

    Poprawka...

--- daut-automata-base/src/test/java/pl/edu/amu/wmi/daut/base/TestAutomatonSpecification.java
+++ daut-automata-base/src/test/java/pl/edu/amu/wmi/daut/base/TestAutomatonSpecification.java
@@ -550,7 +550,7 @@ public class TestAutomatonSpecification extends TestCase {
         zbior = automat.getEpsilonClosure(s2);
         assertTrue(zbior.size() == 1);
 
-        //zbior = automat.getEpsilonClosure(s3);
-        //assertTrue(zbior.size() == 2);
+        zbior = automat.getEpsilonClosure(s3);
+        assertTrue(zbior.size() == 2);
     }
 }
