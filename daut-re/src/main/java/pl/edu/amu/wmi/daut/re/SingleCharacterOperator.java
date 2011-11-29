7a0e5010f49671d06ad6734991b098020291ec72
Adam Beczynski <adam@beczynski>
2011-11-29 21:42:08 +0100

    SingleCharacterOperator - Usunięte zakomentowane bloki kodu z wcześniejszych podejść do zadania. Kto będzie to czytał?

--- daut-re/src/main/java/pl/edu/amu/wmi/daut/re/SingleCharacterOperator.java
+++ daut-re/src/main/java/pl/edu/amu/wmi/daut/re/SingleCharacterOperator.java
@@ -25,28 +25,8 @@ public class SingleCharacterOperator extends NullaryRegexpOperator {
     @Override
     public AutomatonSpecification createFixedAutomaton() {
 
-
-
         AutomatonSpecification automaton = new NaiveAutomatonSpecification();
-
         automaton.makeOneTransitionAutomaton(character);
-
-
-        //generowanie automatu metodą z deterministic utilities dla jednoznakowego stringa.
-        /*
-        Set<String> singleCharLanguage = new HashSet<String>();
-        singleCharLanguage.add(Character.toString(character));
-
-        DeterministicUtilities utility = new DeterministicUtilities();
-        utility.createAutomatonForFiniteLanguage(automaton, singleCharLanguage);//error*/
-
-        //Wersja powielająca kod z "makeOneTransitionAutomaton napisana na poczatku
-        /*State initial = automaton.addState();
-        State characterState = automaton.addState();
-        automaton.markAsInitial(initial);
-        automaton.markAsFinal(characterState);
-        TransitionLabel characterTransition = new CharTransitionLabel(a);
-        automaton.addTransition(initial, characterState, characterTransition);*/
 
         return automaton;           
     }
