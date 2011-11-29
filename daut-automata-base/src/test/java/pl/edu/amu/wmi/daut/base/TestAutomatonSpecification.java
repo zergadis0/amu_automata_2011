18352289eb90ae9f3d68681ced93062b8480c278
Paweł Cińcio <pawel@cincio>
2011-11-30 00:09:16 +0100

    Poprawka4

--- daut-automata-base/src/test/java/pl/edu/amu/wmi/daut/base/TestAutomatonSpecification.java
+++ daut-automata-base/src/test/java/pl/edu/amu/wmi/daut/base/TestAutomatonSpecification.java
@@ -515,9 +515,9 @@ public class TestAutomatonSpecification extends TestCase {
         // i wracanie na ten sam stan).
         NaiveAutomatonSpecification automat = new NaiveAutomatonSpecification();
         // Nowy automat ma już stan początkowy i ustawiamy przejście.
-        automat.addTransition(automat.getInitialState(),
+        /*automat.addTransition(automat.getInitialState(),
                 automat.getInitialState(),
-                new EpsilonTransitionLabel());
+                new EpsilonTransitionLabel());*/
         // Zaznaczamy stan początkowy jako końcowy.
         automat.markAsFinal(automat.getInitialState());
         Set<State> zbior = automat.getEpsilonClosure(automat.getInitialState());
@@ -545,13 +545,10 @@ public class TestAutomatonSpecification extends TestCase {
         automat.addTransition(s3, s0, new EpsilonTransitionLabel());
 
         zbior = automat.getEpsilonClosure(s1);
-        if (zbior != null)
-            assertTrue(zbior.size() == 0);
-        else
-            assertTrue(zbior.size() == 0);
-        zbior = automat.getEpsilonClosure(s2);
         assertTrue(zbior.size() == 1);
-        zbior = automat.getEpsilonClosure(s3);
+        zbior = automat.getEpsilonClosure(s2);
         assertTrue(zbior.size() == 2);
+        zbior = automat.getEpsilonClosure(s3);
+        assertTrue(zbior.size() == 3);
     }
 }
