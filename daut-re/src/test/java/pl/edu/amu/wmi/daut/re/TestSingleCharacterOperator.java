f2104540a0ca5c7ea0c4399b503417ff4aa29e59
Piotr Ożga <ozga@ozga-laptop.(none)>
2011-12-04 20:11:00 +0100

    Fixed TestSingleCharacterOperator

---
+++ daut-re/src/test/java/pl/edu/amu/wmi/daut/re/TestSingleCharacterOperator.java
@@ -0,0 +1,36 @@
+package pl.edu.amu.wmi.daut.re;
+
+import pl.edu.amu.wmi.daut.base.AutomatonSpecification;
+import pl.edu.amu.wmi.daut.base.NaiveAutomatonSpecification;
+
+import junit.framework.TestCase;
+
+
+/**
+ * Testy klasy SingleCharacterOperator.
+ */
+public class SingleCharacterOperator extends TestCase {
+
+    /**
+     * Metoda testujaca konstruktor
+     */
+    public void testSingleCharacterOperator() {
+
+        SingleCharacterOperator operator = new SingleCharacterOperator('c');
+        assertNotNull(operator);
+        assertEquals(operator.Character(),'c');
+    }
+
+    /**
+     * Test metody createFixedAutomaton.
+     */
+    public final void testCreateFixedAutomaton() {
+
+        SingleCharacterOperator operator = new SingleCharacterOperator('c');
+        AutomatonSpecification automaton = operator.createFixedAutomaton();
+        assertFalse(automaton.isEmpty());
+        assertTrue(automaton.isDeterministic());
+        assertEquals(countTransitions(),1);
+
+    }
+}
