25db0da3f0f305cfcd5143cf9fa12b16e9a6c22f
unknown <Administrator@.(none)>
2011-12-06 00:18:28 +0100

    Pierwsz druga

--- daut-re/src/main/java/pl/edu/amu/wmi/daut/re/FixedNumberOfOccurrencesOperator.java
+++ daut-re/src/main/java/pl/edu/amu/wmi/daut/re/FixedNumberOfOccurrencesOperator.java
@@ -2,34 +2,32 @@ package pl.edu.amu.wmi.daut.re;
 
 import java.util.List;
 import pl.edu.amu.wmi.daut.base.AutomatonSpecification;
-import pl.edu.amu.wmi.daut.base.NaiveAutomatonSpecification;
 import pl.edu.amu.wmi.daut.base.EpsilonTransitionLabel;
 import pl.edu.amu.wmi.daut.base.State;
 /**
  * Klasa reprezentującą operator '{n}' z wyrażeń regularnych.
  */
 abstract class FixedNumberOfOccurrencesOperator extends UnaryRegexpOperator{
-    
+
     private int n;
-    
-    public FixedNumberOfOccurrencesOperator(int a)
-    {
+
+    public FixedNumberOfOccurrencesOperator(int a) {
         this.n=a;
     }
-    
+
     public  AutomatonSpecification createAutomatonFromOneAutomaton(
             AutomatonSpecification subautomaton, List<AutomatonSpecification> subautomata) {
-        
+
         List<AutomatonSpecification> lista =null;
         lista=subautomata;
         AutomatonSpecification automatwejsciowy = subautomaton.clone();
-        
+
         AutomatonSpecification automatbudowany = subautomaton.clone();
-        
-        
+
+
         for(int i=0;i<this.n-1;i++) {
             lista.add(i, automatbudowany);
-            
+
             for (State state : automatbudowany.allStates()) {
                 if(automatbudowany.isFinal(state)) {
                             automatbudowany.addTransition(state,
@@ -38,12 +36,9 @@ abstract class FixedNumberOfOccurrencesOperator extends UnaryRegexpOperator{
                     automatbudowany.unmarkAsFinalState(state);
                 }
             }
-            
         }
-        
         return automatbudowany;
-    }
-        
+    }   
 }
     
     
