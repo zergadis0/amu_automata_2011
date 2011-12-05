51bfb8f87dd70d67cd80bab202f1cc747296bc8a
szym1991 <szymon.apolinarski@gmail.com>
2011-12-05 16:59:59 +0100

    pierwsza proba

---
+++ daut-re/src/main/java/pl/edu/amu/wmi/daut/re/RangeNumberOfOccurrencesOperator.java
@@ -0,0 +1,66 @@
+package pl.edu.amu.wmi.daut.re;
+
+import pl.edu.amu.wmi.daut.base.AutomatonSpecification;
+import pl.edu.amu.wmi.daut.base.NaiveAutomatonSpecification;
+import java.util.List;
+import pl.edu.amu.wmi.daut.base.EpsilonTransitionLabel;
+import pl.edu.amu.wmi.daut.base.State;
+
+/**
+ * Klasa reprezentująca operator '{n,m}'.
+ */
+public class RangeNumberOfOccurrencesOperator extends UnaryRegexpOperator {
+    
+    public int max;
+    public int min;
+    
+    /**
+    * Konstruktor klasy.
+    */
+    public RangeNumberOfOccurrencesOperator(int n, int m) {
+       this.max = m;
+       this.min = n;
+   }
+    
+    /**
+    * Glowna metoda klasy.
+    */
+    public AutomatonSpecification createAutomatonFromOneAutomaton(
+            AutomatonSpecification subautomaton) {
+        
+        List<AutomatonSpecification> automata = null;
+        AutomatonSpecification previousAutomaton = new NaiveAutomatonSpecification();
+        previousAutomaton = subautomaton.clone();
+        AutomatonSpecification firstAutomaton = new NaiveAutomatonSpecification();
+        firstAutomaton = subautomaton.clone();
+        firstAutomaton.addTransition(firstAutomaton.getInitialState(), 
+                            previousAutomaton.getInitialState(), 
+                            new EpsilonTransitionLabel());
+        
+        AutomatonSpecification nextAutomaton = new NaiveAutomatonSpecification();
+        nextAutomaton = subautomaton.clone();
+        int counter=1;
+        for(int i=1;i<this.max;i++) {
+            automata.add(i, nextAutomaton);
+            
+            for (State state : previousAutomaton.allStates()) {
+                if(previousAutomaton.isFinal(state)) {
+                    previousAutomaton.addTransition(state, 
+                            automata.get(i).getInitialState(), 
+                            new EpsilonTransitionLabel());
+                }
+            }
+            
+            if(counter < this.min) {
+                for (State state : previousAutomaton.allStates()) {
+                    if(previousAutomaton.isFinal(state)) {
+                        previousAutomaton.unmarkAsFinalState(state);
+                    }
+                }
+            }
+            counter++;
+            previousAutomaton = nextAutomaton;
+        }
+        return firstAutomaton;
+    }
+};
