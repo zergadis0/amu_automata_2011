17af44e06d188993f3cd942656d20f1d6635ab5a
unknown <Administrator@.(none)>
2011-12-06 00:25:03 +0100

    Druga proba

--- daut-re/src/main/java/pl/edu/amu/wmi/daut/re/FixedNumberOfOccurrencesOperator.java
+++ daut-re/src/main/java/pl/edu/amu/wmi/daut/re/FixedNumberOfOccurrencesOperator.java
@@ -7,29 +7,29 @@ import pl.edu.amu.wmi.daut.base.State;
 /**
  * Klasa reprezentującą operator '{n}' z wyrażeń regularnych.
  */
-abstract class FixedNumberOfOccurrencesOperator extends UnaryRegexpOperator{
+abstract class FixedNumberOfOccurrencesOperator extends UnaryRegexpOperator {
 
     private int n;
 
     public FixedNumberOfOccurrencesOperator(int a) {
-        this.n=a;
+        this.n = a;
     }
 
     public  AutomatonSpecification createAutomatonFromOneAutomaton(
             AutomatonSpecification subautomaton, List<AutomatonSpecification> subautomata) {
 
-        List<AutomatonSpecification> lista =null;
-        lista=subautomata;
+        List<AutomatonSpecification> lista = null;
+        lista = subautomata;
         AutomatonSpecification automatwejsciowy = subautomaton.clone();
 
         AutomatonSpecification automatbudowany = subautomaton.clone();
 
 
-        for(int i=0;i<this.n-1;i++) {
+        for (int i = 0; i < this.n-1; i++) {
             lista.add(i, automatbudowany);
 
             for (State state : automatbudowany.allStates()) {
-                if(automatbudowany.isFinal(state)) {
+                if (automatbudowany.isFinal(state)) {
                             automatbudowany.addTransition(state,
                             automatwejsciowy.getInitialState(),
                             new EpsilonTransitionLabel());
