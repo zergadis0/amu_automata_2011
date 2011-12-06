d1a26376ad31fa391d505a2276dbf602ef001a93
heolin123 <heolin123@gmail.com>
2011-12-06 23:59:20 +0100

    Dodaje klase AutomatonUtilities skopiowana od kodera, paulinap.

---
+++ daut-automata-base/src/main/java/pl/edu/amu/wmi/daut/base/AutomatonUtilities.java
@@ -0,0 +1,38 @@
+package pl.edu.amu.wmi.daut.base;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 
+ */
+public class AutomatonUtilities {
+    
+    static Set<Character> getAlphabet(AutomatonSpecification automaton, Set<Character> superset) {
+        
+        Set<Character> alphabet = new HashSet<Character>();
+        
+        for (State s : automaton.allStates()) {
+            for (OutgoingTransition ot : automaton.allOutgoingTransitions(s)) {
+                for (Character c : superset) {
+
+			boolean isAlreadyIn = false;
+			for(Character ch : alphabet)
+                            if(ch == c)	{
+                                isAlreadyIn = true;
+                                break;
+                            }
+
+			if(!isAlreadyIn)
+                            if (ot.getTransitionLabel().canAcceptCharacter(c)) {
+				alphabet.add(c);
+				continue;
+                            }
+		}
+            }
+        }
+        return alphabet;
+                
+    }
+    
+}
