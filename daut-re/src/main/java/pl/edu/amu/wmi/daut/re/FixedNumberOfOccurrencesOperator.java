ed29c7987c69d60ceb4eb1fc1b6f9c50d1c063fa
unknown <Administrator@.(none)>
2011-12-06 12:18:17 +0100

    proba

--- daut-re/src/main/java/pl/edu/amu/wmi/daut/re/FixedNumberOfOccurrencesOperator.java
+++ daut-re/src/main/java/pl/edu/amu/wmi/daut/re/FixedNumberOfOccurrencesOperator.java
@@ -2,7 +2,6 @@ package pl.edu.amu.wmi.daut.re;
 
 import java.util.List;
 import pl.edu.amu.wmi.daut.base.AutomatonSpecification;
-import pl.edu.amu.wmi.daut.base.NaiveAutomatonSpecification;
 import pl.edu.amu.wmi.daut.base.EpsilonTransitionLabel;
 import pl.edu.amu.wmi.daut.base.State;
 
@@ -10,27 +9,26 @@ import pl.edu.amu.wmi.daut.base.State;
 * Klasa reprezentującą operator '{n}' z wyrażeń regularnych.
 */
 abstract class FixedNumberOfOccurrencesOperator extends UnaryRegexpOperator{
-    
+
     private int n;
-    
-    public FixedNumberOfOccurrencesOperator(int a)
-    {
-        this.n=a;
+
+    public FixedNumberOfOccurrencesOperator(int a) {
+        this.n = a;
     }
-    
+
     public AutomatonSpecification createAutomatonFromOneAutomaton(
             AutomatonSpecification subautomaton, List<AutomatonSpecification> subautomata) {
-        
-        List<AutomatonSpecification> lista =null;
-        lista=subautomata;
+
+        List<AutomatonSpecification> lista = null;
+        lista = subautomata;
         AutomatonSpecification automatwejsciowy = subautomaton.clone();
-        
+
         AutomatonSpecification automatbudowany = subautomaton.clone();
-        
-        
-        for(int i=0;i<this.n-1;i++) {
+
+
+        for(int i = 0; i < this.n-1; i++) {
             lista.add(i, automatbudowany);
-            
+
             for (State state : automatbudowany.allStates()) {
                 if(automatbudowany.isFinal(state)) {
                             automatbudowany.addTransition(state,
@@ -39,10 +37,7 @@ abstract class FixedNumberOfOccurrencesOperator extends UnaryRegexpOperator{
                     automatbudowany.unmarkAsFinalState(state);
                 }
             }
-            
         }
-        
         return automatbudowany;
     }
-        
 }
