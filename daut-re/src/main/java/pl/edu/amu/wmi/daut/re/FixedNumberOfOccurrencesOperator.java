1c7c80980304f98c085451dc3c67ad2a2a611250
unknown <Administrator@.(none)>
2011-12-07 22:56:34 +0100

    Rozwiazanie 315

--- daut-re/src/main/java/pl/edu/amu/wmi/daut/re/FixedNumberOfOccurrencesOperator.java
+++ daut-re/src/main/java/pl/edu/amu/wmi/daut/re/FixedNumberOfOccurrencesOperator.java
@@ -3,6 +3,7 @@ package pl.edu.amu.wmi.daut.re;
 import pl.edu.amu.wmi.daut.base.AutomatonSpecification;
 import pl.edu.amu.wmi.daut.base.EpsilonTransitionLabel;
 import pl.edu.amu.wmi.daut.base.State;
+import java.util.LinkedList;
 
 /**
 * Klasa reprezentującą operator '{n}' z wyrażeń regularnych.
@@ -24,21 +25,26 @@ public abstract class FixedNumberOfOccurrencesOperator extends UnaryRegexpOperat
     public AutomatonSpecification createAutomatonFromOneAutomaton(
             AutomatonSpecification subautomaton) {
 
+        LinkedList<AutomatonSpecification> lista = new LinkedList<AutomatonSpecification>();
+
         AutomatonSpecification automatwejsciowy = subautomaton.clone();
 
         AutomatonSpecification automatbudowany = subautomaton.clone();
-
+        
 
         for (int i = 0; i < this.n - 1; i++) {
 
+            lista.add(i,automatwejsciowy);
+
             for (State state : automatbudowany.allStates()) {
                 if (automatbudowany.isFinal(state)) {
-                            automatbudowany.addTransition(state,
-                            automatwejsciowy.getInitialState(),
-                            new EpsilonTransitionLabel());
+                    automatbudowany.addTransition(state,
+                    lista.get(i).getInitialState(),
+                    new EpsilonTransitionLabel());
                     automatbudowany.unmarkAsFinalState(state);
                 }
             }
+            
         }
         return automatbudowany;
     }
