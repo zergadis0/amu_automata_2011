67723ac4b88fb40ba6242157f5489421587c467c
Emilia C <turboelo.2.0@gmail.com>
2011-12-09 16:31:22 +0100

    dodana podklasa Factory

--- daut-re/src/main/java/pl/edu/amu/wmi/daut/re/AnyCharOperator.java
+++ daut-re/src/main/java/pl/edu/amu/wmi/daut/re/AnyCharOperator.java
@@ -2,6 +2,7 @@ package pl.edu.amu.wmi.daut.re;
 
 import pl.edu.amu.wmi.daut.base.AutomatonSpecification;
 import pl.edu.amu.wmi.daut.base.NaiveAutomatonSpecification;
+import java.util.List;
 
 /**
  * Klasa reprezentująca operator '.' z wyrażeń regularnych (dowolny znak).
@@ -18,5 +19,20 @@ public class AnyCharOperator extends NullaryRegexpOperator {
     @Override
     public AutomatonSpecification createFixedAutomaton() {
         return new NaiveAutomatonSpecification().makeOneTransitionAutomaton(character);
+    }
+
+     /**
+     * Fabryka operatora.
+     */
+    public static class Factory extends NullaryRegexpOperatorFactory {
+
+        @Override
+        public int numberOfParams() {
+            return 0;
+        }
+
+        protected RegexpOperator doCreateOperator(List<String> params) {
+            return new AnyCharOperator();
+        }
     }
 }
