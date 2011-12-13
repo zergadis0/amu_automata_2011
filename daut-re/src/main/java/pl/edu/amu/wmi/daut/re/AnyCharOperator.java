f5f4c3c13b068c2f7f99cde9859d70f781a9888d
michal112 <mich_szu@wp.pl>
2011-12-13 18:20:56 +0100

    poprawa

--- daut-re/src/main/java/pl/edu/amu/wmi/daut/re/AnyCharOperator.java
+++ daut-re/src/main/java/pl/edu/amu/wmi/daut/re/AnyCharOperator.java
@@ -7,16 +7,16 @@ import pl.edu.amu.wmi.daut.base.NaiveAutomatonSpecification;
  * Klasa reprezentująca operator '.' z wyrażeń regularnych (dowolny znak).
  */
 public class AnyCharOperator extends NullaryRegexpOperator {
-    private char character;
-
-    /**
-     * Metoda, ustawia pożądany znak.
-     */
-    public void setCharacter(char c) {
-        this.character = c;
-    }
     @Override
     public AutomatonSpecification createFixedAutomaton() {
-        return new NaiveAutomatonSpecification().makeOneTransitionAutomaton(character);
+        AutomatonSpecification automaton = new NaiveAutomatonSpecification();
+        State q0;
+        State q1;
+        automaton.addState(q0);
+        automaton.addState(q1);
+        automaton.addTransition(q0, q1, new AnyTransitionLabel());
+        markAsInitial(q0);
+        markAsFinal(q1);
+        return automaton;
     }
 }
