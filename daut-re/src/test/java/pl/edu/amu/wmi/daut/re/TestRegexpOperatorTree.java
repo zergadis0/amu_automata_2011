e6a5e19e1d27f03a870babb201fa43caadb1af4c
mitofik <mitofik@MWIN7.intranet>
2011-12-14 21:58:04 +0100

    #349 test poczatki

---
+++ daut-re/src/test/java/pl/edu/amu/wmi/daut/re/TestRegexpOperatorTree.java
@@ -0,0 +1,17 @@
+package pl.edu.amu.wmi.daut.re;
+
+import junit.framework.TestCase;
+
+/**
+ * Testy klasy RegexpOperatorTree
+ */
+public class TestRegexpOperatorTree extends TestCase {
+    
+    //(a|b)*
+    public final void Test1() {
+        
+        NullaryRegexpOperatorFactory
+                
+    }
+    
+}
