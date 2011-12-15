06d4ef539bad6f847b5799e84fa61cfb90707911
Pawel Podolski <podolskip@o2.pl>
2011-12-15 15:35:05 +0100

    powinno dzialac

---
+++ daut-re/src/test/java/pl/edu/amu/wmi/daut/re/TestAnyStringOperator.java
@@ -0,0 +1,39 @@
+package pl.edu.amu.wmi.daut.re;
+
+import pl.edu.amu.wmi.daut.base.AutomatonSpecification;
+import pl.edu.amu.wmi.daut.base.NaiveAutomatonSpecification;
+import pl.edu.amu.wmi.daut.base.NondeterministicAutomatonByThompsonApproach;
+import pl.edu.amu.wmi.daut.base.State;
+import pl.edu.amu.wmi.daut.base.CharTransitionLabel;
+import junit.framework.TestCase;
+import pl.edu.amu.wmi.daut.base.AnyTransitionLabel;
+
+
+public class TestAnyStringOperator extends TestCase {
+
+   /**
+    * Test automatów:
+    * spec2 - testuje automat wielu różnych znaków, takze pustego przejscia
+    */
+    public final void testcreateFixedAutomaton() {
+
+
+        AutomatonSpecification spec2 = new NaiveAutomatonSpecification();
+        State q2 = spec2.addState();
+        spec2.addLoop(q2, new AnyTransitionLabel());
+        spec2.markAsInitial(q2);
+        spec2.markAsFinal(q2);
+        NondeterministicAutomatonByThompsonApproach automaton2 =
+                new NondeterministicAutomatonByThompsonApproach(spec2);
+        
+        assertTrue(automaton2.accepts("qwer"));
+        assertTrue(automaton2.accepts("qqqq"));
+        assertTrue(automaton2.accepts("w"));
+        assertTrue(automaton2.accepts("kakademona"));
+        assertTrue(automaton2.accepts("łyżwiaż"));
+        assertTrue(automaton2.accepts("qqrrqqrr"));
+        assertTrue(automaton2.accepts(""));
+        assertTrue(automaton2.accepts("  s  "));
+        
+    }
+}
