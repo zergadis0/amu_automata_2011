e9074bf5d59912714fd7ba2de04df9960f95c039
Elendilll <Arek@.(none)>
2011-12-16 21:26:36 +0100

    10 lub 11 razy Checkstyle

--- daut-re/src/main/java/pl/edu/amu/wmi/daut/re/RegexpUtilities.java
+++ daut-re/src/main/java/pl/edu/amu/wmi/daut/re/RegexpUtilities.java
@@ -8,8 +8,8 @@ import pl.edu.amu.wmi.daut.base.AutomatonSpecification;
 
 public class RegexpUtilities {
 
-	static AutomatonSpecification createAutopmatonFromOperatorTree (RegexpOperatorTree tree) throws RuntimeException
-	{
+    static AutomatonSpecification createAutopmatonFromOperatorTree (RegexpOperatorTree tree) throws RuntimeException
+    {
             //przejdź przez drzewo stanów metodą post-order, przy pomocy dwóch stosów.
             Stack<RegexpOperatorTree> child = new Stack<RegexpOperatorTree>();
             Stack<RegexpOperatorTree> parent = new Stack<RegexpOperatorTree>();
@@ -19,22 +19,22 @@ public class RegexpUtilities {
                 RegexpOperatorTree current = child.peek();
                 parent.push(current);
                 child.pop();
-                
+
                 for (RegexpOperatorTree subTree : current.getSubtrees())
                     child.push(subTree);
             }
-            
+
             //na stosie "parent" mamy teraz wierzchołki w porządku post-order!
             //w porządku post-order chodzi o to, że zawsze zaczynamy od nieodwiedzonych liści
             //i idziemy powoli w kierunku korzenia drzewa.
-            
+
             //utwórz mapę poddrzew na automaty przez nich utworzone.
             Map<RegexpOperatorTree, AutomatonSpecification> map = new HashMap<RegexpOperatorTree, AutomatonSpecification>();
-            
+
             while (!parent.empty())
             {
                 RegexpOperatorTree current = parent.peek();
-                
+
                 //utwórz listę automatów utworzonych przez synów wierzchołka.
                 List<AutomatonSpecification> arguments = new ArrayList<AutomatonSpecification>();
                 for (RegexpOperatorTree subTree : current.getSubtrees())
@@ -45,17 +45,17 @@ public class RegexpUtilities {
                     AutomatonSpecification subTreeAutomaton = map.get(subTree);
                     arguments.add(subTreeAutomaton);
                 }
-                
+
                 //utwórz automat, którego argumentami są automaty wszystkich synów.
                 AutomatonSpecification currentAutomaton = current.getRoot().createAutomaton(arguments);
                 //zapamiętaj automat dla danego wierzchołka. ponieważ liście się
                 //wykonają "najpierw", to nadchodzący po tym rodzice tych liści
-                //będą mieli pełną informację o automatach utworzonych przez 
+                //będą mieli pełną informację o automatach utworzonych przez
                 //swoich synów...
                 map.put(current, currentAutomaton);
-                
+
                 parent.pop();
-                
+
                 //usunęliśmy właśnie wierzchołek-korzeń - zostaliśmy z pustym stosem, możemy zwrócić automat.
                 if (parent.empty())
                     return currentAutomaton;
