f183777adf0b65fb6d18e45af4d237d927ba5781
Kasia <kasia@kasia-Satellite-L300.(none)>
2011-12-18 23:27:31 +0100

    Dodano test do OptionalityOperator

---
+++ daut-re/src/test/java/pl/edu/amu/wmi/daut/re/TestOptionalityOperator.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package pl.edu.amu.wmi.daut.re;
+
+/**
+ *
+ * @author kasia
+ */
+public class TestOptionalityOperator {
+
+}
