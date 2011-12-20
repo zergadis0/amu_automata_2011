e9891b228252d16775810f54d2da46596343480e
wachos <wachos90@gmail.com>
2011-12-20 22:47:59 +0100

    Poprawa

--- daut-re/src/main/java/pl/edu/amu/wmi/daut/re/BellCharacterOperator.java
+++ daut-re/src/main/java/pl/edu/amu/wmi/daut/re/BellCharacterOperator.java
@@ -9,10 +9,10 @@ import pl.edu.amu.wmi.daut.base.NaiveAutomatonSpecification;
  */
 public class BellCharacterOperator extends NullaryRegexpOperator {
 
-    private static final int Number = 7;
+    private static final int NUMBER_SEVEN = 7;
     @Override
     public AutomatonSpecification createFixedAutomaton() {
-        return new NaiveAutomatonSpecification().makeOneTransitionAutomaton(((char) Number));
+        return new NaiveAutomatonSpecification().makeOneTransitionAutomaton(((char) NUMBER_SEVEN));
     }
 
     /**
