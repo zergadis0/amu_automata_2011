709c1fec3e9fd36c94428621347e82a381af360f
unknown <s351764@.labs.wmi.amu.edu.pl>
2011-12-21 11:11:13 +0100

    dodanie komentarzy

--- daut-re/src/main/java/pl/edu/amu/wmi/daut/re/HexSingleCharacterOperator.java
+++ daut-re/src/main/java/pl/edu/amu/wmi/daut/re/HexSingleCharacterOperator.java
@@ -4,11 +4,16 @@ import pl.edu.amu.wmi.daut.base.AutomatonSpecification;
 import pl.edu.amu.wmi.daut.base.NaiveAutomatonSpecification;
 import java.util.List;
 
+/**
+ * Klasa reprezentuj¹c¹ pojedynczy konkretny znak o kodzie Unicode podanym w zapisie szesnastkowym
+ */
 public class HexSingleCharacterOperator extends NullaryRegexpOperator {
 
     private char character;
 
-    //jako parametr przyjmuje char, jaki ma zaakceptowaæ
+    /**
+      * Jako parametr przyjmuje char, jaki ma zaakceptowaæ
+      */
     public HexSingleCharacterOperator(char a) {
         character = a;
     }
@@ -22,6 +27,9 @@ public class HexSingleCharacterOperator extends NullaryRegexpOperator {
         return new NaiveAutomatonSpecification().makeOneTransitionAutomaton(character);
     }
 
+    /**
+      * Fabryka operatora.
+      */
     public static class Factory extends NullaryRegexpOperatorFactory {
 
         @Override
@@ -29,13 +37,15 @@ public class HexSingleCharacterOperator extends NullaryRegexpOperator {
             return 1;
         }
        
-        //jako parametr przyjmuje ci¹g "\x{ABC}", gdzie ABC = znaki 0..9, a..f
+        /**
+          * Jako parametr przyjmuje ci¹g "\x{ABC}", gdzie ABC = znaki 0..9, a..f
+          */
         protected RegexpOperator doCreateOperator(List<String> params) {
             String s = params.get(0);
             s = s.substring(s.indexOf('{') + 1);
             s = s.substring(0, s.indexOf('}'));
             int i = Integer.parseInt(s, 16);
-            char c = (char) i; //wsparcie dla unicode <=3.0
+            char c = (char) i;
             return new SingleCharacterOperator(c);
         }
     }
