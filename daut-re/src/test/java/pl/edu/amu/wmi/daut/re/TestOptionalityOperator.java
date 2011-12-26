a30cc6660481010dd31909055296bbcdffa1b332
Kasia <kasia@kasia-Satellite-L300.(none)>
2011-12-26 19:39:10 +0100

    cosmetic changes in TestOptionalityOperator

--- daut-re/src/test/java/pl/edu/amu/wmi/daut/re/TestOptionalityOperator.java
+++ daut-re/src/test/java/pl/edu/amu/wmi/daut/re/TestOptionalityOperator.java
@@ -19,34 +19,34 @@ public class TestOptionalityOperator extends TestCase {
      */
     public final void testCreateAutomatonFromOneAutomaton() {
 
-    AutomatonSpecification automaton = new NaiveAutomatonSpecification();
+        AutomatonSpecification automaton = new NaiveAutomatonSpecification();
 
-    State q0 = automaton.addState();
-    State q1 = automaton.addState();
-    State q2 = automaton.addState();
-    automaton.addTransition(q0, q1, new CharTransitionLabel('a'));
-    automaton.addTransition(q1, q2, new CharTransitionLabel('b'));
-    automaton.addLoop(q1, new CharTransitionLabel('a'));
-    automaton.addLoop(q2, new CharTransitionLabel('b'));
+        State q0 = automaton.addState();
+        State q1 = automaton.addState();
+        State q2 = automaton.addState();
+        automaton.addTransition(q0, q1, new CharTransitionLabel('a'));
+        automaton.addTransition(q1, q2, new CharTransitionLabel('b'));
+        automaton.addLoop(q1, new CharTransitionLabel('a'));
+        automaton.addLoop(q2, new CharTransitionLabel('b'));
 
-    automaton.markAsInitial(q0);
-    automaton.markAsFinal(q2);
+        automaton.markAsInitial(q0);
+        automaton.markAsFinal(q2);
 
-    OptionalityOperator operator = new OptionalityOperator();
-    NondeterministicAutomatonByThompsonApproach result =
-        new NondeterministicAutomatonByThompsonApproach(
-            operator.createAutomatonFromOneAutomaton(automaton));
+        OptionalityOperator operator = new OptionalityOperator();
+        NondeterministicAutomatonByThompsonApproach result =
+            new NondeterministicAutomatonByThompsonApproach(
+                operator.createAutomatonFromOneAutomaton(automaton));
 
-    assertTrue(result.accepts("aaabbb"));
-    assertTrue(result.accepts("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabbbbb"));
-    assertTrue(result.accepts("aabbbbbbbbbbbbb"));
-    assertTrue(result.accepts("ab"));
-    assertTrue(automaton.acceptEmptyWord());
+        assertTrue(result.accepts("aaabbb"));
+        assertTrue(result.accepts("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabbbbb"));
+        assertTrue(result.accepts("aabbbbbbbbbbbbb"));
+        assertTrue(result.accepts("ab"));
+        assertTrue(automaton.acceptEmptyWord());
 
-    assertFalse(result.accepts("aaabbbbaabbbbab"));
-    assertFalse(result.accepts("aaaaaaaaaaa"));
-    assertFalse(result.accepts("cojapacze"));
-    assertFalse(result.accepts("marcinmateuszhanc"));
-    assertFalse(result.accepts("jestemautomateminieakceptuje"));
+        assertFalse(result.accepts("aaabbbbaabbbbab"));
+        assertFalse(result.accepts("aaaaaaaaaaa"));
+        assertFalse(result.accepts("cojapacze"));
+        assertFalse(result.accepts("macrimnatueshzanc"));
+        assertFalse(result.accepts("jestemautomateminieakceptuje"));
     }
 }
