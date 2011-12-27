b5de145d06405ee32dc3a2b0785d5d4895eef0b2
unknown <Administrator@.(none)>
2011-12-27 18:28:28 +0100

    proba 3

--- daut-re/src/test/java/pl/edu/amu/wmi/daut/re/TestNewLineOperator.java
+++ daut-re/src/test/java/pl/edu/amu/wmi/daut/re/TestNewLineOperator.java
@@ -20,11 +20,13 @@ public class TestNewLineOperator extends TestCase {
         AutomatonSpecification spec = operator.createFixedAutomaton();
         assertFalse(spec.isEmpty());
         assertFalse(spec.acceptEmptyWord());
-        assertTrue(spec.isDeterministic());
-        assertEquals(spec.countTransitions(), 1);
 
         AutomatonByRecursion automaton = new AutomatonByRecursion(spec);
         assertTrue(automaton.accepts("\n"));
+        assertFalse(automaton.accepts("a"));
+        assertFalse(automaton.accepts("%"));
+        assertFalse(automaton.accepts("\t"));
+        assertFalse(automaton.accepts(" "));
 
     }
 
@@ -34,7 +36,6 @@ public class TestNewLineOperator extends TestCase {
     public void testFactory() {
 
         Factory factory = new Factory();
-        assertEquals(factory.numberOfParams(), 0);
         RegexpOperator operator2 = factory.createOperator(new ArrayList<String>());
         int arity = operator2.arity();
         assertEquals(arity, 0);
