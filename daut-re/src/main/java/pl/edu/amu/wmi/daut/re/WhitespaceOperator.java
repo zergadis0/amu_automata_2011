cf861b7ad6d1e73cbedf9c340d22fa5b4d56eb20
cole1911 <damian.jozefowski@gmail.com>
2011-12-28 18:36:11 +0100

    WhitespaceOperator(poprawa błędów)

--- daut-re/src/main/java/pl/edu/amu/wmi/daut/re/WhitespaceOperator.java
+++ daut-re/src/main/java/pl/edu/amu/wmi/daut/re/WhitespaceOperator.java
@@ -43,7 +43,7 @@ public class WhitespaceOperator extends NullaryRegexpOperator {
         }
 
         @Override
-     protected RegexpOperator doCreateOperator(List<String> params) {
+        protected RegexpOperator doCreateOperator(List<String> params) {
             return new WhitespaceOperator();
         }
     }
