1a15e42c2e8874f4db7cdbbdfcc8cc061e1d2d72
iwanicki <oliwer.iwanicki@gmail.com>
2012-01-02 21:48:47 +0100

    usuniecie spacji na koncu linii

--- daut-re/src/main/java/pl/edu/amu/wmi/daut/re/NoWhitespaceOperator.java
+++ daut-re/src/main/java/pl/edu/amu/wmi/daut/re/NoWhitespaceOperator.java
@@ -19,7 +19,7 @@ public class NoWhitespaceOperator extends NullaryRegexpOperator {
         AutomatonSpecification noWhitespaceAutomaton = new NaiveAutomatonSpecification();
         State state1 = noWhitespaceAutomaton.addState();
         State state2 = noWhitespaceAutomaton.addState();
-        noWhitespaceAutomaton.addTransition(state1, state2, 
+        noWhitespaceAutomaton.addTransition(state1, state2,
               new ComplementCharClassTransitionLabel(LIST_OF_WHITESPACE_CHARS));
         noWhitespaceAutomaton.markAsInitial(state1);
         noWhitespaceAutomaton.markAsFinal(state2);
