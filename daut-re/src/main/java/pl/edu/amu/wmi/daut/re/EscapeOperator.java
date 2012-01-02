14e9ee371f971c208e23795219a0c95236a0eef1
Tutanchamon <tutenchamon@gmail.com>
2012-01-03 00:55:58 +0100

    Pierwsza wersja EscapeOperator (issue 463)

---
+++ daut-re/src/main/java/pl/edu/amu/wmi/daut/re/EscapeOperator.java
@@ -0,0 +1,54 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pl.edu.amu.wmi.daut.re;
+
+import pl.edu.amu.wmi.daut.base.AutomatonSpecification;
+import pl.edu.amu.wmi.daut.base.NaiveAutomatonSpecification;
+
+/**
+ *
+ * Klasa EscapeOperator reprezentująca znak poprzedzony znakiem ucieczki
+ * (w wyrażeniach regularnych POSIX - odwrócony ukośnik).
+ */
+public class EscapeOperator extends NullaryRegexpOperator {
+
+    private char znak;
+    
+    @Override
+    public AutomatonSpecification createFixedAutomaton() {
+        switch (znak){
+            case 'n':
+                return new NaiveAutomatonSpecification().makeOneTransitionAutomaton('\n');
+            case 't':
+                return new NaiveAutomatonSpecification().makeOneTransitionAutomaton('\t');
+       
+            case 'a':
+                return new NaiveAutomatonSpecification().makeOneTransitionAutomaton(('\7'));
+                
+            case 'f':
+                return new NaiveAutomatonSpecification().makeOneTransitionAutomaton('\f');
+                
+            case 'r':
+                return new NaiveAutomatonSpecification().makeOneTransitionAutomaton('\r');
+                
+            case 'v':
+                return new NaiveAutomatonSpecification().makeOneTransitionAutomaton('\13');
+                
+             default:
+                return new NaiveAutomatonSpecification().makeOneTransitionAutomaton(znak);
+            
+                
+        }
+        
+    }
+    
+public EscapeOperator(char a) {
+    this.setChar(a);
+        
+    }
+private void setChar(char b){
+    this.znak = b;
+}
+}
