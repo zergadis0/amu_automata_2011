1c6ca7e08b9734091c396255d0fe7e92568cab9e
iwanicki <oliwer.iwanicki@gmail.com>
2012-01-02 19:24:47 +0100

    usuniecie spacji w NoWhitespaceOperator

--- daut-re/src/main/java/pl/edu/amu/wmi/daut/re/NoWhitespaceOperator.java
+++ daut-re/src/main/java/pl/edu/amu/wmi/daut/re/NoWhitespaceOperator.java
@@ -18,7 +18,7 @@ public class NoWhitespaceOperator extends NullaryRegexpOperator {
         AutomatonSpecification noWhitespaceAutomaton = new NaiveAutomatonSpecification();
         State state1 = noWhitespaceAutomaton.addState();
         State state2 = noWhitespaceAutomaton.addState();
-        noWhitespaceAutomaton.addTransition(state1, state2, 
+        noWhitespaceAutomaton.addTransition(state1, state2,
               new ComplementCharClassTransitionLabel("\t\n\f\r \u000B"));
         noWhitespaceAutomaton.markAsInitial(state1);
         noWhitespaceAutomaton.markAsFinal(state2);
