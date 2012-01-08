5b1f5bc5a022a0a756f10252bd4ba1fe9397d955
irmina90 <weronika.zietek@gmail.com>
2012-01-08 23:14:56 +0100

    poprawka7

--- daut-automata-base/src/main/java/pl/edu/amu/wmi/daut/base/EndOfTextTransitionLabel.java
+++ daut-automata-base/src/main/java/pl/edu/amu/wmi/daut/base/EndOfTextTransitionLabel.java
@@ -23,7 +23,7 @@ public class EndOfTextTransitionLabel extends ZeroLengthConditionalTransitionLab
         }
         return false;
     }
-    
+
     @Override
     public boolean canAcceptCharacter(char c) {
         return false;
