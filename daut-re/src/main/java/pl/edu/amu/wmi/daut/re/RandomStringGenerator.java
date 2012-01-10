bcc90eaa65ba9e8fd513e4d6e9f4d9d139733933
blackrose1 <bedzie.padac@gmail.com>
2012-01-10 17:23:59 +0100

    poprawka

--- daut-re/src/main/java/pl/edu/amu/wmi/daut/re/RandomStringGenerator.java
+++ daut-re/src/main/java/pl/edu/amu/wmi/daut/re/RandomStringGenerator.java
@@ -10,13 +10,30 @@ public class RandomStringGenerator {
 
     private static final double lambda = 0.1;
 
-    public RandomStringGenerator(String alphabet) {
-        this.alphabet = alphabet;
-        random = new Random();
-        if (alphabet == null) {
-            throw new IllegalArgumentException("Alphabet can be empty - but cannot be null.");
+public RandomStringGenerator(String alphabet) {
+    random = new Random();
+    if (alphabet == null) {
+        throw new IllegalArgumentException("Alphabet can be empty - but cannot be null.");
+    }
+    StringBuilder alphabetNonRepetitive = new StringBuilder();
+    for (int i = 0; i < alphabet.length(); i++) {
+        char character = alphabet.charAt(i);
+        if (!doesContains(alphabetNonRepetitive.toString(), character)) {
+            alphabetNonRepetitive.append(character);
         }
     }
+    this.alphabet = alphabetNonRepetitive.toString();
+}
+
+private boolean doesContains(String str, char characterToFind) {
+    for (int i = 0; i < str.length(); i++) {
+        char character = str.charAt(i);
+        if (character.equals(characterToFind) {
+            return true;
+        }
+    }
+    return false;
+}
 
     public String getRandomString() {
         StringBuffer sb = new StringBuffer();
