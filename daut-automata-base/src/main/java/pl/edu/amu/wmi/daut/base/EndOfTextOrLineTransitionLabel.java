93bb56d53cf251c945397394de51db3e688eeeb0
Kamil Kuczkowiak <s362643@pc17g.labs.wmi.amu.edu.pl>
2012-01-11 10:41:10 +0100

    Checkstyle

--- daut-automata-base/src/main/java/pl/edu/amu/wmi/daut/base/EndOfTextOrLineTransitionLabel.java
+++ daut-automata-base/src/main/java/pl/edu/amu/wmi/daut/base/EndOfTextOrLineTransitionLabel.java
@@ -11,10 +11,10 @@ public class EndOfTextOrLineTransitionLabel extends ZeroLengthConditionalTransit
 
     @Override
     protected boolean doCheckContext(String s, int position) {
-        if (( s.length() < position ) || (position < 0)) {
+        if ((s.length() < position) || (position < 0)) {
             throw new PositionOutOfStringBordersException();
         }
-        if ((position == s.length()) || ( s.charAt(position) == '\n')) {
+        if ((position == s.length()) || (s.charAt(position) == '\n')) {
             return true;
         }
         return false;
