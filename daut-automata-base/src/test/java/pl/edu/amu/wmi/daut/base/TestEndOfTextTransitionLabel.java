ffdb34f6eee81ee9c92e52242431c12e15ea542f
Piotr Ożga <ozga@ozga-laptop.(none)>
2012-01-12 10:11:56 +0100

    added test case

--- daut-automata-base/src/test/java/pl/edu/amu/wmi/daut/base/TestEndOfTextTransitionLabel.java
+++ daut-automata-base/src/test/java/pl/edu/amu/wmi/daut/base/TestEndOfTextTransitionLabel.java
@@ -16,6 +16,20 @@ public class TestEndOfTextTransitionLabel extends TestCase {
         assertTrue(trans.doCheckContext("trololololo", 11));
         assertTrue(trans.doCheckContext("masa", 4));
 
+        try {
+            trans.doCheckContext("cojapacze", 4);
+            fail();
+        } catch (PositionOutOfStringBordersException e) {
+            assertTrue(true);
+        }
+
+        try {
+            trans.doCheckContext("jemzupe", 27);
+            fail();
+        } catch (PositionOutOfStringBordersException e) {
+            assertTrue(true);
+        }
+
         /**
          * Test atrybutów.
          */
