6f1ea0d6a7314779f4c153c6c3edd0ae7f0d9da5
unknown <a@a-PC.(none)>
2012-01-13 22:45:13 +0100

    czesc testow

---
+++ daut-automata-base/src/test/java/pl/edu/amu/wmi/daut/base/TestReallyCheckingContextChecker.java
@@ -0,0 +1,24 @@
+package pl.edu.amu.wmi.daut.base;
+
+import junit.framework.TestCase;
+
+/**
+ * Test klasy ReallyCheckingContextChecker.
+ */
+public class TestReallyCheckingContextChecker extends TestCase {
+
+    /**
+     * Prosty test ReallyCheckingContextChecker.
+     */
+    public final void testReallyCheckingContextChecker() {
+
+        ReallyCheckingContextChecker contextChecker = new ReallyCheckingContextChecker("napis",1);
+
+        assertFalse(contextChecker.check(new EndOfTextTransitionLabel()));
+
+        contextChecker = new ReallyCheckingContextChecker("napis" + '\u0003', 5);
+
+        assertTrue(contextChecker.check(new EndOfTextTransitionLabel()));
+
+    }
+}
