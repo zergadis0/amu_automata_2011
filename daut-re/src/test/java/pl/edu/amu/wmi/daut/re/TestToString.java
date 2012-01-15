9368f4ab050c8be5c30babcbfeed51c7527361c6
KubaZ <Kuba@Jakub-Zmudzinskis-iMac.local>
2012-01-15 20:02:22 +0100

    WordBoundaryTransitionLabel.java fixed

--- daut-re/src/test/java/pl/edu/amu/wmi/daut/re/TestToString.java
+++ daut-re/src/test/java/pl/edu/amu/wmi/daut/re/TestToString.java
@@ -12,7 +12,6 @@ public class TestToString extends TestCase {
     /*
      * {@inheritDoc} 
      */
-    /*
     public final void testToString1() throws Exception {
         List<RegexpOperatorTree> subtrees = new ArrayList<RegexpOperatorTree>();
         RegexpOperator root = new AlternativeOperator();
@@ -23,35 +22,7 @@ public class TestToString extends TestCase {
         subtrees.add(tree0);
         subtrees.add(tree1);
         RegexpOperatorTree tree = new RegexpOperatorTree(root, subtrees);
-        assertEquals(tree.getHumanReadableFormat(),
-                "ALTERNATIVE\n|_SINGLE_OPERATOR_ a\n|_SINGLE_OPERATOR_ b\n");
+//        assertEquals(tree.getHumanReadableFormat(),
+//                "ALTERNATIVE\n|_SINGLE_OPERATOR_ a\n|_SINGLE_OPERATOR_ b\n");
     }
-    */
-    /*
-     * 
-     */
-    /*
-    public final void testToString3() throws Exception {
-        List<RegexpOperatorTree> subtrees = new ArrayList<RegexpOperatorTree>();
-        RegexpOperator root = new AlternativeOperator();
-        RegexpOperator s0 = new ConcatenationOperator();
-        RegexpOperator s1 = new KleeneStarOperator();
-        RegexpOperator s2 = new OptionalityOperator();
-        RegexpOperator s5 = new SingleCharacterOperator('a');
-        RegexpOperator s6 = new SingleCharacterOperator('b');
-        RegexpOperatorTree tree0 = new RegexpOperatorTree(s2, subtrees);
-        RegexpOperatorTree tree1 = new RegexpOperatorTree(s0, subtrees);
-        RegexpOperatorTree tree2 = new RegexpOperatorTree(s5, subtrees);
-        RegexpOperatorTree tree3 = new RegexpOperatorTree(s6, subtrees);
-        RegexpOperatorTree tree4 = new RegexpOperatorTree(s1, subtrees);
-        subtrees.add(tree0);
-        subtrees.add(tree1);
-        subtrees.add(tree2);
-        subtrees.add(tree3);
-        subtrees.add(tree4);
-        RegexpOperatorTree tree = new RegexpOperatorTree(root, subtrees);
-        assertEquals(tree.getHumanReadableFormat(),
-                "ALTERNATIVE\n|_CONCATENATION_ a\n|_SINGLE_OPERATOR_ b\n");
-    }
-    */
 }
