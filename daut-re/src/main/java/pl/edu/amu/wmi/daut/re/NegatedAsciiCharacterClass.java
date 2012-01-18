44c62447cdb9d3cc866102d05c4c4ca4ad81278a
jakub <jakub@jakub-HP>
2012-01-18 11:24:27 +0100

    Drobne poprawki.

--- daut-re/src/main/java/pl/edu/amu/wmi/daut/re/NegatedAsciiCharacterClass.java
+++ daut-re/src/main/java/pl/edu/amu/wmi/daut/re/NegatedAsciiCharacterClass.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package pl.edu.amu.wmi.daut.re;
 
 import java.util.List;
@@ -68,6 +64,6 @@ public class NegatedAsciiCharacterClass extends NullaryRegexpOperator {
      */
     @Override
     public String toString() {
-        return "ASCII";
+        return "^ASCII";
     }
 }
